ad0654d2a4f12abb77effdaaba27c155558005b9
Davide Motte <37443982+dmotte@users.noreply.github.com>
2021-07-15 19:21:33 +0200

    Added resize-vids

---
+++ resize-vids/README.md
@@ -0,0 +1,7 @@
+# resize-vids
+
+Small **bash script** to resize mp4 videos using the ffmpeg utility.
+
+Run ./resize-vids.sh --help for usage information.
+
+TODO
---
+++ resize-vids/resize-vids.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+# Usage example: ./resize-vids.sh *.mp4
+
+#OUTPUT_WIDTH=854
+#OUTPUT_HEIGHT=480
+OUTPUT_WIDTH=640
+OUTPUT_HEIGHT=360
+
+for i in "$@"; do
+    INPUT_FILENAME="$i"
+    OUTPUT_FILENAME="${i}-resized.mp4"
+
+    ffmpeg -i "$INPUT_FILENAME" \
+        -vf "scale=$OUTPUT_WIDTH:$OUTPUT_HEIGHT:force_original_aspect_ratio=decrease,pad=$OUTPUT_WIDTH:$OUTPUT_HEIGHT:(ow-iw)/2:(oh-ih)/2" \
+        "$OUTPUT_FILENAME"
+done
