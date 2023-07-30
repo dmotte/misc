bd58c1916ff2c35621d41ec600d8a357edf75194
dmotte <37443982+dmotte@users.noreply.github.com>
2023-07-30 14:26:29 +0200

    Improved ffmpeg-resize-* script parameters

--- scripts/ffmpeg-resize-imgs.sh
+++ scripts/ffmpeg-resize-imgs.sh
@@ -2,14 +2,13 @@
 
 set -e
 
-p_ratio='1/2'
+: "${RATIO:=1/2}"
 
 # Create a directory for the output files
 mkdir -p resized/
 
 for i in "$@"; do
-    bn="$(basename "$i")"  # Basename
-    o="resized/$bn"        # Output filename
+    o="resized/$(basename "$i")" # Output filename
 
-    ffmpeg -i "$i" -vf "scale=iw*$p_ratio:ih*$p_ratio" "$o"
+    ffmpeg -i "$i" -vf "scale=iw*$RATIO:ih*$RATIO" "$o"
 done
--- scripts/ffmpeg-resize-vids.sh
+++ scripts/ffmpeg-resize-vids.sh
@@ -2,19 +2,18 @@
 
 set -e
 
-p_outw=640  # Output width
-p_outh=360  # Output height
-#p_outw=854
-#p_outh=480
+: "${OUTW:=640}" # Output width
+: "${OUTH:=360}" # Output height
+# : "${OUTW:=854}"
+# : "${OUTH:=480}"
 
 # Create a directory for the output files
 mkdir -p resized/
 
 for i in "$@"; do
-    bn="$(basename "$i")"  # Basename
-    o="resized/$bn"        # Output filename
+    o="resized/$(basename "$i")" # Output filename
 
     ffmpeg -i "$i" \
-        -vf "scale=$p_outw:$p_outh:force_original_aspect_ratio=decrease,pad=$p_outw:$p_outh:(ow-iw)/2:(oh-ih)/2" \
+        -vf "scale=$OUTW:$OUTH:force_original_aspect_ratio=decrease,pad=$OUTW:$OUTH:(ow-iw)/2:(oh-ih)/2" \
         "$o"
 done
