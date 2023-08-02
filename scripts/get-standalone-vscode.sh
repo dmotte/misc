a986e4b4f88a35d690af0b4d2e1d2a50a4822e71
dmotte <37443982+dmotte@users.noreply.github.com>
2023-08-03 00:57:00 +0200

    Added launcher to get-standalone-vscode.sh

--- scripts/get-standalone-vscode.sh
+++ scripts/get-standalone-vscode.sh
@@ -8,6 +8,7 @@ set -e
 VSCODE_DIR_PATH="$HOME/apps/vscode"
 VSCODE_ARCHIVE_URL='https://code.visualstudio.com/sha/download?build=stable&os=linux-x64'
 VSCODE_ARCHIVE_PATH="$VSCODE_DIR_PATH/archive.tar.gz"
+VSCODE_LAUNCHER_PATH="$HOME/.local/share/applications/vscode.desktop"
 
 if [ -d "$VSCODE_DIR_PATH" ]; then
     echo "Directory $VSCODE_DIR_PATH already exists. Installed app version:"
@@ -30,5 +31,15 @@ tar -xzf "$VSCODE_ARCHIVE_PATH" -C "$VSCODE_DIR_PATH"
 
 echo 'Installed app version:'
 "$VSCODE_DIR_PATH/VSCode-linux-x64/bin/code" -v
+
+cat << EOF >> "$VSCODE_LAUNCHER_PATH"
+[Desktop Entry]
+Name=Visual Studio Code
+Exec=$VSCODE_DIR_PATH/VSCode-linux-x64/code %f
+Comment=Visual Studio Code
+Terminal=false
+Icon=$VSCODE_DIR_PATH/VSCode-linux-x64/resources/app/resources/linux/code.png
+Type=Application
+EOF
 
 echo 'Installation completed successfully'
