4ddf3cc4995f46cbc36d7aacf22a3905839a29a9
dmotte <37443982+dmotte@users.noreply.github.com>
2023-08-05 02:53:56 +0200

    Added check to git-check-branches.sh

--- scripts/git-check-branches.sh
+++ scripts/git-check-branches.sh
@@ -5,6 +5,8 @@ set -e
 for i in "$@"; do
     n=$(git -C "$i" for-each-ref --format='%(objectname)' refs/heads | \
         sort | uniq | wc -l)
+    if [ $n -ne 1 ]; then echo "$i: $n different branches"; fi
 
-    if [ $n -ne 1 ]; then echo "$i: $n"; fi
+    branch="$(git -C "$i" rev-parse --abbrev-ref HEAD)"
+    if [ "$branch" != main ]; then echo "$i: on $branch"; fi
 done
