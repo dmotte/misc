62cd27e9b8ae1e176208eee50d83c5bde1d1dcdd
dmotte <37443982+dmotte@users.noreply.github.com>
2023-10-01 14:47:24 +0200

    Use fetch --all in git-fetch-and-status

--- scripts/git-fetch-and-status.sh
+++ scripts/git-fetch-and-status.sh
@@ -6,7 +6,7 @@ for i in "$@"; do
     echo -n "### $i: "
     ( # Subshell
         cd $i
-        git fetch
+        git fetch --all # Fetches all branches from all remotes
         git status -bs
     )
 done
