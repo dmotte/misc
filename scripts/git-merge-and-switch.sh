0380c615972afc56a721ac6d9129cc6fe57e763b
dmotte <37443982+dmotte@users.noreply.github.com>
2023-10-01 15:40:35 +0200

    Set branch_final to the initial one

--- scripts/git-merge-and-switch.sh
+++ scripts/git-merge-and-switch.sh
@@ -4,13 +4,14 @@ set -e
 
 branch_from=dev
 branch_to=main
-branch_final=main
 
 for i in "$@"; do
     echo -e "### \033[0;35m$i\033[0m:"
 
     ( # Subshell
         cd $i
+
+        branch_final="$(git rev-parse --abbrev-ref HEAD)"
 
         if git switch "$branch_from"; then
             git merge "$branch_to"
