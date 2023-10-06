f0d41ccc7c8582b0d4ce8cb15a379664abfc86f4
dmotte <37443982+dmotte@users.noreply.github.com>
2023-10-07 01:56:14 +0200

    Check for default branch in git-check-branches.sh

--- scripts/git-check-branches.sh
+++ scripts/git-check-branches.sh
@@ -7,6 +7,10 @@ for i in "$@"; do
         sort | uniq | wc -l)
     if [ "$n" -ne 1 ]; then echo "$i: $n different branches"; fi
 
-    branch="$(git -C "$i" rev-parse --abbrev-ref HEAD)"
-    if [ "$branch" != main ]; then echo "$i: on $branch"; fi
+    branch_default="$(git -C "$i" symbolic-ref refs/remotes/origin/HEAD | \
+        sed 's@^refs/remotes/origin/@@')"
+    branch_current="$(git -C "$i" rev-parse --abbrev-ref HEAD)"
+    if [ "$branch_current" != "$branch_default" ]; then
+        echo "$i: on $branch_current"
+    fi
 done
