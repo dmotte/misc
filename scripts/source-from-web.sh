6885c2af72a307db805c3458abc6232a94d0a48a
dmotte <37443982+dmotte@users.noreply.github.com>
2023-10-08 12:48:50 +0200

    Transformed source-from-web in a Bash script

---
+++ scripts/source-from-web.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+fetch_and_check() {
+    local content; content="$(curl -fsSL "$1")" && \
+    if [ "$(echo "$content" | sha256sum | cut -d' ' -f1)" = "$2" ]
+        then echo "$content"
+        else echo "Checksum verification failed for $1" >&2; return 1
+    fi
+}
+
+################################ USAGE EXAMPLE #################################
+
+script_content="$(fetch_and_check \
+    'https://.../myscript.sh' \
+    '1a2b3c4d1a2b3c4d1a2b3c4d1a2b3c4d1a2b3c4d1a2b3c4d1a2b3c4d1a2b3c4d')"
+
+script_content="$(echo "$script_content" | sed 's/my-old-text/my-new-text/')"
+
+echo "$script_content"
+# . <(echo "$script_content") # To run the script in the current environment
+# bash <(echo "$script_content") # To run the script in a separate shell
