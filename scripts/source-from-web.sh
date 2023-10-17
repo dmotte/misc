68ebaa6776ab6eaafb5107867c066b241f75f015
dmotte <37443982+dmotte@users.noreply.github.com>
2023-10-18 00:24:24 +0200

    Fixed fetch_and_check newline handling

--- scripts/source-from-web.sh
+++ scripts/source-from-web.sh
@@ -2,12 +2,25 @@
 
 set -e
 
+# This function aims to be as compact as possible, and compatible with the Dash
+# shell (/bin/sh)
+
+# This uses the "local myvar; myvar=..." approach instead of "local myvar=..."
+# directly, because the "local" builtin command always returns 0 upon
+# successful variable creation. See https://stackoverflow.com/a/9525025
+
+# This puts a dummy "x" character at the end of the content variable to
+# correctly preserve newlines, because the $(...) command substitution trims
+# trailing newline characters. See https://stackoverflow.com/a/15184414
+
+# Variables: c = content, s = checksum
+
 fetch_and_check() {
-    local content; content="$(curl -fsSL "$1")" && \
-    if [ "$(echo -n "$content" | sha256sum | cut -d' ' -f1)" = "$2" ]
-        then echo -n "$content"
-        else echo "Checksum verification failed for $1" >&2; return 1
-    fi
+    local c s; c="$(curl -fsSL "$1"; echo x)" && \
+    s="$(echo -n "${c%x}" | sha256sum | cut -d' ' -f1)" && \
+    if [ "$s" = "$2" ]; then echo -n "${c%x}"
+    else echo "Checksum verification failed for $1: got $s, expected $2" >&2
+    return 1; fi
 }
 
 ################################ USAGE EXAMPLE #################################
