03cce480d9b87b2f32abf17a4d6fe7edac3c2af6
dmotte <37443982+dmotte@users.noreply.github.com>
2023-11-14 00:40:12 +0100

    Added provisioning/disable-ipv6.sh script

---
+++ scripts/provisioning/disable-ipv6.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+if [ "$EUID" != '0' ]; then
+    echo 'This script must be run as root' >&2
+    exit 1
+fi
+
+options=$(getopt -o n -l no-reload -- "$@")
+eval set -- "$options"
+
+reload=y
+
+while :; do
+    case "$1" in
+    -n|--no-reload) reload=n;;
+    --) shift; break;;
+    esac
+    shift
+done
+
+cat << 'EOF' > /etc/sysctl.d/99-disable-ipv6.conf
+net.ipv6.conf.all.disable_ipv6=1
+net.ipv6.conf.default.disable_ipv6=1
+net.ipv6.conf.lo.disable_ipv6=1
+EOF
+
+if [ "$reload" = y ]; then sysctl --system; fi
