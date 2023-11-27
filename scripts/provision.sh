4d25ed7fc91722ffc7e9f839e982cf0a0b9116b0
dmotte <37443982+dmotte@users.noreply.github.com>
2023-11-27 21:57:05 +0100

    Fixed provisioning script for different dir names

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -5,17 +5,18 @@ set -e
 # Provision a remote server using a Bash script
 # Usage example: ./provision.sh provisioning/ user@hostname -p2222
 
-prov_dir="$1"; shift
+cd "$1"; shift
 
-if [ ! -f "$prov_dir/main.sh" ]; then
+if [ ! -f main.sh ]; then
     echo 'File main.sh not found' >&2
     exit 1
 fi
 
-tar -cvzf- "$prov_dir" | ssh "$@" '
+tar -cvzf- . | ssh "$@" '
     set -ex
     rm -rf /tmp/provisioning
-    tar -xvzf- -C/tmp
+    mkdir /tmp/provisioning
+    tar -xvzf- -C/tmp/provisioning
     bash /tmp/provisioning/main.sh || result="$?"
     rm -rf /tmp/provisioning
     exit "${result:-0}"
