6d5472cdc14546af1c7612dc7216b8063554b371
dmotte <37443982+dmotte@users.noreply.github.com>
2023-11-29 23:48:54 +0100

    Implemented return code in git-check-branches.sh

--- scripts/git-check-branches.sh
+++ scripts/git-check-branches.sh
@@ -2,15 +2,19 @@
 
 set -e
 
+result=0
+
 for arg; do
     n=$(git -C "$arg" for-each-ref --format='%(objectname)' refs/heads | \
         sort | uniq | wc -l)
-    if [ "$n" -ne 1 ]; then echo "$arg: $n different branches"; fi
+    if [ "$n" -ne 1 ]; then echo "$arg: $n different branches"; result=1; fi
 
     branch_default="$(git -C "$arg" symbolic-ref refs/remotes/origin/HEAD | \
         sed 's@^refs/remotes/origin/@@')"
     branch_current="$(git -C "$arg" rev-parse --abbrev-ref HEAD)"
     if [ "$branch_current" != "$branch_default" ]; then
-        echo "$arg: on $branch_current"
+        echo "$arg: on $branch_current"; result=1
     fi
 done
+
+exit "$result"
