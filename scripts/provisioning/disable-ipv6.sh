d48253754ad4f29e014c6014857ae876305a67b6
dmotte <37443982+dmotte@users.noreply.github.com>
2024-01-06 15:44:22 +0100

    Simplified disable-ipv6.sh

--- scripts/provisioning/disable-ipv6.sh
+++ scripts/provisioning/disable-ipv6.sh
@@ -7,23 +7,10 @@ if [ "$EUID" != '0' ]; then
     exit 1
 fi
 
-options=$(getopt -o n -l no-reload -- "$@")
-eval set -- "$options"
-
-reload=y
-
-while :; do
-    case "$1" in
-        -n|--no-reload) reload=n;;
-        --) shift; break;;
-    esac
-    shift
-done
-
 cat << 'EOF' > /etc/sysctl.d/99-disable-ipv6.conf
 net.ipv6.conf.all.disable_ipv6=1
 net.ipv6.conf.default.disable_ipv6=1
 net.ipv6.conf.lo.disable_ipv6=1
 EOF
 
-if [ "$reload" = y ]; then sysctl --system; fi
+if [ "$SYSCTL_RELOAD" = 'true' ]; then sysctl --system; fi
