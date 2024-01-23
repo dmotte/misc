4dd06eb09c6da0d3f9ffa7cf554083638d7c2954
dmotte <37443982+dmotte@users.noreply.github.com>
2024-01-23 01:14:29 +0100

    Added git-line-ages.sh

---
+++ scripts/git-line-ages.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+# This script produces a report about your Git repo in which, for each line of
+# code, the timestamp it was written is reported
+
+# Usage example: ./git-line-ages.sh | LC_ALL=C sort -t, -k1
+
+[ -z "$1" ] || cd "$1"
+
+# Lists all non-empty regular (no symlinks) text files. See
+# https://stackoverflow.com/a/24350112
+git grep -Il '' | while read -r file; do
+    git blame -t "$file" | sed -E \
+        's|^.+\(.+\s+([0-9]+)\s+[+0-9]+\s+([0-9]+)\) (.*)$|\1,'"$file"',\2,\3|'
+done
