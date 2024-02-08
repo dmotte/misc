db240f2b0817c107de29007570433761f0eeab10
dmotte <37443982+dmotte@users.noreply.github.com>
2024-02-08 22:18:07 +0100

    Added msgbuf-url and msgbuf-checksum args and service manager check to setup-lognot.sh

--- scripts/provisioning/setup-lognot.sh
+++ scripts/provisioning/setup-lognot.sh
@@ -12,13 +12,15 @@ set -e
 [ "$EUID" = 0 ] || { echo 'This script must be run as root' >&2; exit 1; }
 
 options=$(getopt -o s:i:m:b:c: -l service-manager: -l source-cmd: \
+    -l msgbuf-url -l msgbuf-checksum \
     -l msgbuf-interval: -l msgbuf-max-msg-len: -l bot-token: -l chat-id: \
     -l supervisor-priority: -l systemd-restartsec: -l systemd-wantedby: -- "$@")
 eval "set -- $options"
 
 service_manager=auto
 source_cmd=''
-# TODO msgbuf_url and msgbuf_checksum
+msgbuf_url="https://github.com/dmotte/msgbuf/releases/latest/download/msgbuf-$(uname -m)-unknown-linux-gnu"
+msgbuf_checksum='3fcec4e61ef0fdbc9e4a703ba3c5b3075b20336d57b963e05676ccdab3ad5ca4'
 msgbuf_interval=60 # seconds
 msgbuf_max_msg_len=2048 # bytes
 bot_token='' # Telegram bot token
@@ -31,6 +33,8 @@ while :; do
     case "$1" in
         --service-manager) shift; service_manager="$1";;
         -s|--source-cmd) shift; source_cmd="$1";;
+        --msgbuf-url) shift; msgbuf_url="$1";;
+        --msgbuf-checksum) shift; msgbuf_checksum="$1";;
         -i|--msgbuf-interval) shift; msgbuf_interval="$1";;
         -m|--msgbuf-max-msg-len) shift; msgbuf_max_msg_len="$1";;
         -b|--bot-token) shift; bot_token="$1";;
@@ -43,8 +47,10 @@ while :; do
     shift
 done
 
+[[ "$service_manager" =~ ^(auto|supervisor|systemd)$ ]] || \
+    { echo "Unsupported service manager: $service_manager" >&2; exit 1; }
+
 # TODO error if source_cmd or bot_token or chat_id are empty
-# TODO error if service manager is not supported
 
 apt_update_if_old() {
     if [ -z "$(find /var/lib/apt/lists -maxdepth 1 -mmin -60)" ]; then
