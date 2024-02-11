bea4df993795f8db7bc69e273c5771ee9d4d7006
dmotte <37443982+dmotte@users.noreply.github.com>
2024-02-11 04:17:12 +0100

    Added systemd-cron.sh

---
+++ scripts/provisioning/systemd-cron.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+set -e
+
+# This script can be used to set up a systemd-based "cron job" consisting of a
+# timer and a oneshot service
+
+[ "$EUID" = 0 ] || { echo 'This script must be run as root' >&2; exit 1; }
+
+options=$(getopt -o n:e:w:c: -l name:,event-expr:,workdir:,command: -- "$@")
+eval "set -- $options"
+
+name='' # Warning: some characters are forbidden. See the code below
+event_expr=''
+workdir=''
+command='' # Warning: some characters are forbidden. See the code below
+
+while :; do
+    case "$1" in
+        -n|--name) shift; name="$1";;
+        -e|--event-expr) shift; event_expr="$1";;
+        -w|--workdir) shift; workdir="$1";;
+        -c|--command) shift; command="$1";;
+        --) shift; break;;
+    esac
+    shift
+done
+
+[[ "$name" =~ ^[0-9A-Za-z-]+$ ]] || { echo "Invalid name: $name" >&2; exit 1; }
+
+[ -n "$event_expr" ] || { echo 'Event expression cannot be empty' >&2; exit 1; }
+
+[ -n "$command" ] || { echo 'Command cannot be empty' >&2; exit 1; }
+[[ "$command" != *$'\n'* ]] || \
+    { echo 'Command contains invalid characters' >&2; exit 1; }
+
+################################################################################
+
+if [ -n "$workdir" ]; then line_workdir="WorkingDirectory=$workdir"; fi
+
+cat << EOF > "/etc/systemd/system/$name.service"
+[Unit]
+Description=$name service
+After=network.target network-online.target systemd-networkd.service NetworkManager.service connman.service
+
+[Service]
+Type=oneshot
+
+$line_workdir
+ExecStart=$command
+EOF
+
+cat << EOF > "/etc/systemd/system/$name.timer"
+[Unit]
+Description=$name timer
+
+[Timer]
+OnCalendar=$event_expr
+Persistent=true
+
+[Install]
+WantedBy=timers.target
+EOF
+
+systemctl daemon-reload; systemctl enable "$name.timer"
+
+################################################################################
+
+if [ "$SYSTEMD_TIMER_RESTART" = 'true' ]; then
+    systemctl restart "$name.timer"
+fi
