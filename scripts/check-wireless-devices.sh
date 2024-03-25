b849354eeedde75cae86717a2a2a7f84d9ce86b7
dmotte <37443982+dmotte@users.noreply.github.com>
2024-03-26 00:20:31 +0100

    Added scripts/check-wireless-devices.sh

---
+++ scripts/check-wireless-devices.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+echo 'Checking that no wireless devices are present'
+
+text=$(bash -c 'lspci; lsusb; lsmod; ip a; rfkill list' 2>&1 || :)
+if echo "$text" | grep -Ei 'wireless|wifi|wi-fi|wlan|802\.11|80211|bluetooth'; then
+    echo 'Some wireless device(s) found' >&2; exit 1
+fi
+text=$(systemctl status bluetooth 2>&1 || :)
+if [ "$text" != 'Unit bluetooth.service could not be found.' ]; then
+    echo 'The bluetooth.service unit may be running' >&2; exit 1
+fi
