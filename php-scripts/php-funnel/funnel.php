b556805318629a6671daed86d3ad7f00e4a313d2
dmotte <37443982+dmotte@users.noreply.github.com>
2024-05-11 14:29:11 +0200

    Added initial-scale=1.0

--- php-scripts/php-funnel/funnel.php
+++ php-scripts/php-funnel/funnel.php
@@ -113,7 +113,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <title>Funnel</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <style type="text/css">
         body,
