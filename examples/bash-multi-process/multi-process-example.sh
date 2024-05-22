f5188fcfe53fc931a4e2c96794a208921528f9df
dmotte <37443982+dmotte@users.noreply.github.com>
2024-05-22 02:43:33 +0200

    Added examples/bash-multi-process

---
+++ examples/bash-multi-process/multi-process-example.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+# Example of how to spawn multiple concurrent processes in Bash and display
+# both their stdout and stderr (with proper prefixes)
+
+sample_service() {
+    local name=${1:?}; shift
+    while :; do
+        sleep "$(shuf -i0-5 -n1)"; echo "This is stdout of $name"
+        sleep "$(shuf -i0-5 -n1)"; echo "This is stderr of $name" >&2
+    done
+}
+
+run_with_prefixes() {
+    local name=${1:?}; shift
+    { "$@" \
+        > >(while read -r i; do echo "[$name:out] $i"; done) \
+        2> >(while read -r i; do echo "[$name:err] $i"; done >&2)
+    } 2>&1
+}
+
+################################################################################
+
+trap 'kill $(jobs -p)' EXIT
+
+echo 'Starting service myfirstsvc'
+run_with_prefixes myfirstsvc sample_service myfirstsvc &
+echo 'Starting service mysecondsvc'
+run_with_prefixes mysecondsvc sample_service mysecondsvc &
+
+wait # until all jobs finish
+trap - EXIT
