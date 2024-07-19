7e159061528971c6f1b721e7fce98ffa80df0dec
dmotte <37443982+dmotte@users.noreply.github.com>
2024-07-19 02:08:50 +0200

    Added edit-sftp-file.sh

---
+++ scripts/edit-sftp-file.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+# This script can be used to quickly edit a remote text file on an SFTP server
+
+# Usage example:
+#   ./edit-sftp-file.sh user@127.0.0.1:myfile.txt scp -P2022
+
+editor=${EDITOR:-}
+
+[ -n "$editor" ] || for i in /usr/bin/{nano,vi,vim}; do
+    [ -f "$i" ] && { editor=$i; break; }
+done
+
+[ -n "$editor" ] || { echo 'Cannot find a valid text editor' >&2; exit 1; }
+
+scp_remote_file=${1:?} scp_app=${2:?}; shift 2
+scp_args=("$@")
+
+################################################################################
+
+tmpdir=$(mktemp -d --tmpdir edit-sftp-file-XXXXXXXXXX)
+trap 'rm -rf $tmpdir' EXIT
+
+scp_local_file=$tmpdir/tmp-file
+
+echo "Downloading file to $scp_local_file"
+"$scp_app" "${scp_args[@]}" "$scp_remote_file" "$scp_local_file"
+"$editor" "$scp_local_file"
+echo 'Uploading file'
+"$scp_app" "${scp_args[@]}" "$scp_local_file" "$scp_remote_file"
