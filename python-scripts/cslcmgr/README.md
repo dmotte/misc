60d00896ca2a5a516400ad4335239aff7c9ad861
dmotte <37443982+dmotte@users.noreply.github.com>
2024-08-15 13:11:14 +0200

    Added cslcmgr

---
+++ python-scripts/cslcmgr/README.md
@@ -0,0 +1,48 @@
+# cslcmgr
+
+**C**ode**S**pace **L**ife**C**ycle **M**ana**g**e**r**: a **Web API server** (with optional **Web UI frontend**) that can be used to **start and stop GitHub Codespaces**.
+
+This is meant to be run locally on your PC, or behind a reverse proxy with **authentication**.
+
+## Usage
+
+First of all you need to generate one or more **GitHub PATs** (_Personal Access Tokens_) to be able to control your Codespaces using the GitHub API. The tokens must have the following **permissions**:
+
+- "**Codespaces**" repository permissions (**read**)
+- "**Codespaces lifecycle admin**" repository permissions (**write**)
+
+> **Note**: for more information on why these permissions are needed, see https://docs.github.com/en/rest/codespaces/codespaces?apiVersion=2022-11-28
+
+Set up a **Python venv** (virtual environment) and install some packages inside it:
+
+```bash
+python3 -mvenv venv
+venv/bin/python3 -mpip install -r requirements.txt
+```
+
+Then set the necessary configuration **environment variables**. Example:
+
+```bash
+export CSLCMGR_UI=true \
+    CSLCMGR_IDS=mycs01,mycs02 \
+    CSLCMGR_NAMES=happy-ananas-a1b2c3d4,yellow-wrench-a1b2c3d4 \
+    CSLCMGR_TOKENS=github_pat_XXXXX,github_pat_XXXXX
+```
+
+> **Note**: for more details about the supported environment variables, see the `get_config` function [in the code](app.py).
+
+Then you can run the app either in a **development** or **production** configuration.
+
+**Development**:
+
+```bash
+CSLCMGR_LOG_LEVEL=DEBUG venv/bin/python3 app.py
+```
+
+**Production**:
+
+```bash
+venv/bin/python3 -mgunicorn --access-logfile=- -w4 -b127.0.0.1:8000 'app:create_app()'
+```
+
+> **Note**: for more information about running a _Flask_ application with _Gunicorn_, see https://flask.palletsprojects.com/en/3.0.x/deploying/gunicorn/
---
+++ python-scripts/cslcmgr/app.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+
+import os
+import requests
+import sys
+
+from types import SimpleNamespace
+from flask import Flask, jsonify, send_file, redirect
+
+GITHUB_API_VERSION = '2022-11-28'
+
+
+def get_config():
+    cfg_lists = {
+        k: [] if v is None else v.split(',')
+        for k, v in {
+            # Custom internal IDs of the codespaces. You can freely choose
+            # these, but they have to be unique
+            'ids': os.getenv('CSLCMGR_IDS', None),
+            # Names of the codespaces
+            'names': os.getenv('CSLCMGR_NAMES', None),
+            # Tokens for each codespace
+            'tokens': os.getenv('CSLCMGR_TOKENS', None),
+        }.items()
+    }
+
+    len_ids = len(cfg_lists['ids'])
+
+    if len_ids <= 0:
+        raise Exception('No codespaces defined')
+    if len(cfg_lists['names']) != len_ids:
+        raise Exception('IDs and names lists length mismatch')
+    if len(cfg_lists['tokens']) != len_ids:
+        raise Exception('IDs and tokens lists length mismatch')
+
+    return SimpleNamespace(
+        # Logging level
+        log_level=os.getenv('CSLCMGR_LOG_LEVEL', 'INFO'),
+        # Whether or not to enable the Web UI (frontend)
+        ui=os.getenv('CSLCMGR_UI', 'false') == 'true',
+
+        codespaces={
+            id: SimpleNamespace(
+                name=cfg_lists['names'][i],
+                token=cfg_lists['tokens'][i],
+            )
+            for i, id in enumerate(cfg_lists['ids'])
+        },
+    )
+
+
+def github_api_call(cfg, app, req_method, url_suffix, id, callback):
+    if id not in cfg.codespaces:
+        return jsonify({'message': 'Codespace not found'}), 404
+
+    resp = req_method(
+        f'https://api.github.com/user/codespaces/{cfg.codespaces[id].name}{url_suffix}',
+        headers={
+            'Accept': 'application/vnd.github+json',
+            'Authorization': f'Bearer {cfg.codespaces[id].token}',
+            'X-GitHub-Api-Version': GITHUB_API_VERSION,
+        })
+
+    try:
+        resp.raise_for_status()
+        return callback(resp)
+    except requests.exceptions.HTTPError as e:
+        app.logger.error('HTTPError during GitHub API request: %s', e)
+        return jsonify({'message': 'Server error'}), 500
+
+
+def create_app():
+    cfg = get_config()
+
+    if cfg.ui:
+        app = Flask(__name__)
+
+        app.add_url_rule('/', 'root',
+                         lambda: redirect('/static/'))
+        app.add_url_rule('/static/', 'static_index',
+                         lambda: send_file('static/index.html'))
+    else:
+        app = Flask(__name__, static_folder=None)
+
+    app.logger.setLevel(cfg.log_level)
+
+    app.logger.debug('Configuration: %s', cfg)
+
+    @app.route('/list', endpoint='list')
+    def route_list():
+        return jsonify({'codespaces': list(cfg.codespaces.keys())}), 200
+
+    @app.route('/state/<id>', endpoint='state')
+    def route_state(id):
+        return github_api_call(
+            cfg, app, requests.get, '', id,
+            lambda resp: (jsonify({
+                'id': id,
+                'state': resp.json()['state'],
+            }), 200)
+        )
+
+    @app.route('/start/<id>', methods=['POST'], endpoint='start')
+    def route_start(id):
+        return github_api_call(
+            cfg, app, requests.post, '/start', id,
+            lambda resp: (jsonify({'message': 'OK'}), 200)
+        )
+
+    @app.route('/stop/<id>', methods=['POST'], endpoint='stop')
+    def route_stop(id):
+        return github_api_call(
+            cfg, app, requests.post, '/stop', id,
+            lambda resp: (jsonify({'message': 'OK'}), 200)
+        )
+
+    return app
+
+
+def main():
+    # Run the Flask local development server if the script is executed directly
+    create_app().run()
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
