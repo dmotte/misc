69fdef7dc68c9138bad36b02a882e6e41d018461
dmotte <37443982+dmotte@users.noreply.github.com>
2024-08-21 00:04:40 +0200

    Added support for other platforms to get-standalone-vscode.sh

--- scripts/get-standalone-vscode.sh
+++ scripts/get-standalone-vscode.sh
@@ -3,10 +3,13 @@
 set -e
 
 # To run this script without downloading it:
-# bash <(curl -fsSL https://raw.githubusercontent.com/dmotte/misc/main/scripts/get-standalone-vscode.sh)
+# bash <(curl -fsSL https://raw.githubusercontent.com/dmotte/misc/main/scripts/get-standalone-vscode.sh) linux-x64
+
+# Ensure that some variables are defined
+: "${1:?}"
 
 main_dir=${STANDALONE_VSCODE_MAIN_DIR:-$HOME/apps/vscode}
-archive_url=${STANDALONE_VSCODE_ARCHIVE_URL:-https://code.visualstudio.com/sha/download?build=stable&os=linux-x64}
+archive_url="https://code.visualstudio.com/sha/download?build=stable&os=$1"
 archive_path=$main_dir/archive.tar.gz
 
 launcher_default=~/.local/share/applications/vscode.desktop
@@ -20,7 +23,7 @@ fi
 
 if [ -d "$main_dir" ]; then
     echo "Directory $main_dir already exists. Installed app version:"
-    "$main_dir/VSCode-linux-x64/bin/code" -v
+    "$main_dir/VSCode-$1/bin/code" -v
 
     main_dir_old=$main_dir-old-$(date -u +%Y-%m-%d-%H%M%S)
     echo "Moving $main_dir to $main_dir_old"
@@ -36,7 +39,7 @@ echo "Extracting $archive_path to $main_dir"
 tar -xzf "$archive_path" -C "$main_dir"
 
 echo 'Installed app version:'
-"$main_dir/VSCode-linux-x64/bin/code" -v
+"$main_dir/VSCode-$1/bin/code" -v
 
 ################################################################################
 
@@ -45,10 +48,10 @@ if [ -n "$launcher" ]; then
     install -m644 /dev/stdin "$launcher" << EOF
 [Desktop Entry]
 Name=Visual Studio Code
-Exec=$main_dir/VSCode-linux-x64/code %f
+Exec=$main_dir/VSCode-$1/code %f
 Comment=Visual Studio Code
 Terminal=false
-Icon=$main_dir/VSCode-linux-x64/resources/app/resources/linux/code.png
+Icon=$main_dir/VSCode-$1/resources/app/resources/linux/code.png
 Type=Application
 EOF
 fi
