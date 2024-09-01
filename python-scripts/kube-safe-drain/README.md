476b84339d036994e274064fec34cbdefd9a9b39
dmotte <37443982+dmotte@users.noreply.github.com>
2024-09-01 13:42:12 +0200

    Added Python script kube-safe-drain

---
+++ python-scripts/kube-safe-drain/README.md
@@ -0,0 +1,25 @@
+# kube-safe-drain
+
+This script can be used to **safely drain** all the **cordoned nodes** in a _Kubernetes_ cluster, by performing all the necessary **rollout restarts**.
+
+## Usage
+
+First you have to **cordon** all the nodes you want to drain. Example:
+
+```bash
+kubectl cordon mynode
+```
+
+Then **generate** the list of `rollout restart` commands:
+
+```bash
+kubectl get -A node,pod,cj,deploy,ds,job,rs,sts -ojson | python3 main.py
+```
+
+**Review** the list and **run** the commands. The rollout restarts **may take some time**, even after the commands have completed the execution.
+
+Finally, **check** that the nodes have been drained successfully. Example:
+
+```bash
+kubectl drain --ignore-daemonsets --delete-emptydir-data --dry-run=client mynode
+```
---
+++ python-scripts/kube-safe-drain/main.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+
+
+CONTROLLER_KINDS_SKIP = ['CronJob', 'DaemonSet']
+CONTROLLER_KINDS_ACCEPT = ['Deployment', 'StatefulSet']
+
+
+def leaves(tree: dict, key_children: str, key_get: str):
+    if len(tree[key_children]) == 0:
+        return [tree[key_get]]
+
+    return [
+        leave
+        for child in tree[key_children]
+        for leave in leaves(child, key_children, key_get)
+    ]
+
+
+def res2ref(res: dict):
+    '''
+    Converts a resource dict into a resource reference, i.e. a dict with only
+    namespace, kind and name
+    '''
+    return {
+        'namespace': res['metadata'].get('namespace'),
+        'kind': res['kind'],
+        'name': res['metadata']['name'],
+    }
+
+
+def resolve_ref(resources: list, ref: dict):
+    '''
+    Given a resource reference (a dict with namespace, kind and name), scans
+    the resources list to find the associated resource
+    '''
+    matches = [
+        r for r in resources
+        if r['metadata'].get('namespace') == ref['namespace']
+        and r['kind'] == ref['kind']
+        and r['metadata']['name'] == ref['name']
+    ]
+    if len(matches) == 0:
+        raise LookupError(f'No resource found for reference {ref}')
+    if len(matches) > 1:
+        raise LookupError(f'Multiple resources found for reference {ref}')
+    return matches[0]
+
+
+def controllers_tree(resources: list, res: dict):
+    '''
+    Given a resource dict, recursively builds the tree of all its controllers
+    '''
+    return {'resource': res, 'controllers': [
+        controllers_tree(resources, resolve_ref(resources, {
+            'namespace': res['metadata']['namespace'],
+            'kind': ownref['kind'],
+            'name': ownref['name'],
+        }))
+        for ownref in res['metadata'].get('ownerReferences', [])
+        if ownref['controller']
+    ]}
+
+
+def workloads_to_restart(resources: list, kinds_skip: list, kinds_accept: list):
+    '''
+    Gets the list of the Kubernetes workloads that need to be restarted, i.e.
+    the workloads that have at least one pod running on a cordoned node
+    '''
+
+    cordoned_nodes_names = [
+        r['metadata']['name'] for r in resources
+        if r['kind'] == 'Node' and r['spec'].get('unschedulable')
+    ]
+
+    if len(cordoned_nodes_names) == 0:
+        raise LookupError(f'There are no cordoned nodes in the cluster')
+
+    workloads = {}
+
+    for node_name in cordoned_nodes_names:
+        for pod in [
+            r for r in resources
+            if r['kind'] == 'Pod' and r['spec']['nodeName'] == node_name
+        ]:
+            for ctrl in leaves(controllers_tree(resources, pod),
+                               'controllers', 'resource'):
+                ns = ctrl['metadata']['namespace']
+                kind = ctrl['kind']
+                name = ctrl['metadata']['name']
+
+                if kind in kinds_skip:
+                    continue
+                if kind not in kinds_accept:
+                    raise ValueError(f'Invalid controller kind {kind}')
+
+                id = f'{kind} {ns}/{name}'
+                if id not in workloads:
+                    workloads[id] = ctrl
+
+    return workloads.values()
+
+
+def main():
+    input = json.load(sys.stdin)
+
+    workloads = workloads_to_restart(
+        input['items'], CONTROLLER_KINDS_SKIP, CONTROLLER_KINDS_ACCEPT)
+
+    for workload in workloads:
+        wlref = res2ref(workload)
+        ns, kind, name = wlref['namespace'], wlref['kind'], wlref['name']
+        print(f'kubectl rollout restart -n {ns} {kind} {name}')
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
