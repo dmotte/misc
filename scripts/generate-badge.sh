ef6d1234b577fe5f4d4a288961aee40ffd0a3abe
dmotte <37443982+dmotte@users.noreply.github.com>
2024-11-19 17:52:00 +0100

    Minified output of generate-badge.sh

--- scripts/generate-badge.sh
+++ scripts/generate-badge.sh
@@ -10,12 +10,13 @@ readonly emoji=${1:?} text=${2:?} color_fg=${3:-fff} color_bg=${4:-555}
 readonly width_text=$((${#text} * 6))
 readonly width_total=$((32 + width_text))
 
-cat << EOF
-<svg xmlns="http://www.w3.org/2000/svg" width="$width_total" height="20">
-    <rect width="$width_total" height="20" fill="#$color_bg"/>
-    <g fill="#$color_fg" font-family="Consolas,monospace" font-size="11">
-        <text x="4" y="14">$emoji</text>
-        <text x="24" y="14" textLength="$width_text">$text</text>
-    </g>
-</svg>
-EOF
+echo -n '<svg xmlns="http://www.w3.org/2000/svg" width="'"$width_total"'" height="20">'
+
+echo -n '<rect width="'"$width_total"'" height="20" fill="#'"$color_bg"'"/>'
+
+echo -n '<g fill="#'"$color_fg"'" font-family="Consolas,monospace" font-size="11">'
+echo -n '<text x="4" y="14">'"$emoji"'</text>'
+echo -n '<text x="24" y="14" textLength="'"$width_text"'">'"$text"'</text>'
+echo -n '</g>'
+
+echo -n '</svg>'
