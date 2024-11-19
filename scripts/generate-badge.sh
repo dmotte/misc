36364cdb40b244ebacf3acbb9887661138f5e26d
dmotte <37443982+dmotte@users.noreply.github.com>
2024-11-19 01:26:03 +0100

    Implemented textLength into generate-badge.sh

--- scripts/generate-badge.sh
+++ scripts/generate-badge.sh
@@ -7,11 +7,15 @@ set -e
 
 readonly emoji=${1:?} text=${2:?} color_fg=${3:-fff} color_bg=${4:-555}
 
-readonly width=$((32 + ${#text} * 6))
+readonly width_text=$((${#text} * 6))
+readonly width_total=$((32 + width_text))
 
 cat << EOF
-<svg xmlns="http://www.w3.org/2000/svg" width="$width" height="20">
-    <rect width="$width" height="20" fill="#$color_bg"/>
-    <text x="4" y="14" fill="#$color_fg" font-family="Consolas,monospace" font-size="11">$emoji $text</text>
+<svg xmlns="http://www.w3.org/2000/svg" width="$width_total" height="20">
+    <rect width="$width_total" height="20" fill="#$color_bg"/>
+    <g fill="#$color_fg" font-family="Consolas,monospace" font-size="11">
+        <text x="4" y="14">$emoji</text>
+        <text x="24" y="14" textLength="$width_text">$text</text>
+    </g>
 </svg>
 EOF
