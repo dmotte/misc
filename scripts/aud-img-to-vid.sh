d24a462c0001b2b148314d185ff925eb226d2918
dmotte <37443982+dmotte@users.noreply.github.com>
2024-12-10 14:04:11 +0100

    Fixed defaults in aud-img-to-vid.sh

--- scripts/aud-img-to-vid.sh
+++ scripts/aud-img-to-vid.sh
@@ -10,7 +10,7 @@ set -e
 # Usage example: ./aud-img-to-vid.sh myfile.{jpg,mp3,mp4}
 
 readonly in_img=${1:?} in_aud=${2:?} out_vid=${3:?} \
-    out_width=${4:1920} out_height=${5:1080}
+    out_width=${4:-1920} out_height=${5:-1080}
 
 ffmpeg -loop 1 -i "$in_img" -i "$in_aud" \
     -c:v libx264 -c:a aac -b:a 192k -shortest \
