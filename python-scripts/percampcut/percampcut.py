9833827bf487e3fd908771eeba54b920e5d3798c
dmotte <37443982+dmotte@users.noreply.github.com>
2024-12-11 19:26:17 +0100

    Moved percampcut logic to function

--- python-scripts/percampcut/percampcut.py
+++ python-scripts/percampcut/percampcut.py
@@ -10,6 +10,65 @@ from contextlib import ExitStack
 from pydub import AudioSegment
 
 
+def compute_values(audio: AudioSegment, perc_clipping: float = 0.0001,
+                   level_start: float = 0.0005,
+                   level_end: float = 0.0005) -> dict:
+    if perc_clipping < 0 or perc_clipping > 1:
+        raise ValueError(f'Invalid perc_clipping value: {perc_clipping}. It '
+                         'must be between 0 and 1, inclusive')
+    if level_start < 0 or level_start > 1:
+        raise ValueError(f'Invalid level_start value: {level_start}. It '
+                         'must be between 0 and 1, inclusive')
+    if level_end < 0 or level_end > 1:
+        raise ValueError(f'Invalid level_end value: {level_end}. It '
+                         'must be between 0 and 1, inclusive')
+
+    ############################################################################
+
+    # Maximum possible (absolute) value of a sample
+    max_poss = 2 ** (8 * audio.sample_width - 1)
+
+    samples = audio.get_array_of_samples()
+    len_samples = len(samples)
+
+    samples_abs = [abs(s) for s in samples]
+
+    # Similar to an ECDF (Empirical Cumulative Distribution Function)
+    sorted_abs = samples_abs.copy()
+    sorted_abs.sort()
+
+    # Maximum sample value in the audio track
+    max_abs = sorted_abs[-1]
+
+    # Value of the sample(s) that will be the new maximum after the track will
+    # be amplified
+    target_max = sorted_abs[round((len_samples - 1) * (1 - perc_clipping))]
+
+    if target_max == 0:
+        raise ValueError('The computed target_max is zero (maybe '
+                         'perc_clipping is a little bit too high?)')
+
+    # Gain expressed as multiplication factor (e.g. 2.0 -> 2x)
+    gain_factor = max_poss / target_max
+    # Gain expressed in decibels
+    gain_db = 20 * math.log(gain_factor, 10)
+
+    ############################################################################
+
+    # TODO calculate start and end
+
+    ############################################################################
+
+    return {
+        'max_poss': max_poss,
+        'len_samples': len_samples,
+        'max_abs': max_abs,
+        'target_max': target_max,
+        'gain_factor': gain_factor,
+        'gain_db': gain_db,
+    }
+
+
 def main(argv=None):
     if argv is None:
         argv = sys.argv
@@ -33,9 +92,9 @@ def main(argv=None):
                         help='Percentage of audio samples that are allowed '
                         'to clip (default: 0.0001)')
 
-    parser.add_argument('-l', '--min-level-start', type=float, default=0.0005,
+    parser.add_argument('-l', '--level-start', type=float, default=0.0005,
                         help='TODO (default: 0.0005)')
-    parser.add_argument('-L', '--min-level-end', type=float, default=0.0005,
+    parser.add_argument('-L', '--level-end', type=float, default=0.0005,
                         help='TODO (default: 0.0005)')
 
     args = parser.parse_args(argv[1:])
@@ -48,36 +107,11 @@ def main(argv=None):
         file_out = (sys.stdout if args.file_out == '-'
                     else stack.enter_context(open(args.file_out, 'wb')))
 
-        p: float = args.perc_clipping  # TODO check that 0 <= p <= 1
-        print('p', p)
-
         audio: AudioSegment = AudioSegment.from_file(file_in)
 
-        max_possible_value = 2 ** (8 * audio.sample_width - 1)
-        print('max_possible_value', max_possible_value)
-
-        samples: array = audio.get_array_of_samples()
-
-        len_samples = len(samples)
-        print('len_samples', len_samples)
-
-        samples_abs = [abs(s) for s in samples]
-
-        # Similar to an ECDF (Empirical Cumulative Distribution Function)
-        sorted_abs = samples_abs.copy()
-        sorted_abs.sort()
-
-        max_abs = sorted_abs[-1]
-        print('max_abs', max_abs)
-
-        v = sorted_abs[round((len_samples - 1) * (1 - p))]
-        print('v', v)
-
-        a = max_possible_value / v  # TODO prevent division by zero
-        print('a', a)
-
-        decibels = 20 * math.log(a, 10)
-        print('decibels', decibels)
+        # TODO fix this
+        print(compute_values(audio, args.perc_clipping, args.level_start,
+                             args.level_end), file=file_out)
 
     return 0
 
