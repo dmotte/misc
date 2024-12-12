8cd8e4b80c6f7831d8f0bbec73065d9940659eda
dmotte <37443982+dmotte@users.noreply.github.com>
2024-12-12 23:57:03 +0100

    Fixed codecs in aud-img-to-vid and related ffmpeg snippet

--- scripts/aud-img-to-vid.sh
+++ scripts/aud-img-to-vid.sh
@@ -13,6 +13,6 @@ readonly in_img=${1:?} in_aud=${2:?} out_vid=${3:?} \
     out_width=${4:-1920} out_height=${5:-1080}
 
 ffmpeg -loop 1 -i "$in_img" -i "$in_aud" \
-    -c:v libx264 -c:a aac -b:a 192k -shortest \
+    -c:a copy -shortest \
     -vf "scale=$out_width:$out_height:force_original_aspect_ratio=decrease,pad=$out_width:$out_height:(ow-iw)/2:(oh-ih)/2" \
     "$out_vid"
