57a797144a9b3d75b6c5428fd7564f12390edd2f
dmotte <37443982+dmotte@users.noreply.github.com>
2024-12-18 23:31:55 +0100

    Added readonly to arg vars in github-get-all-repos.sh

--- scripts/github-get-all-repos.sh
+++ scripts/github-get-all-repos.sh
@@ -5,8 +5,7 @@ set -e
 # Usage example:
 # bash <(curl -fsSL https://raw.githubusercontent.com/dmotte/misc/main/scripts/github-get-all-repos.sh) users/octocat '.archived == false and .fork == false' | while read -r i; do git -C "$(basename "$i")" pull || git clone --depth=1 "git@github.com:$i.git"; done
 
-owner=${1:?}
-filter=${2:-true}
+readonly owner=${1:?} filter=${2:-true}
 
 if [ "$owner" = "${owner#users/}" ] && [ "$owner" = "${owner#orgs/}" ]; then
     echo 'Invalid owner specified' >&2; exit 1
