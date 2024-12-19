1b1b122dc3d2f7a7f232b065049a3153d65e0a37
dmotte <37443982+dmotte@users.noreply.github.com>
2024-12-19 02:54:45 +0100

    Improved error msgs and checks in MotteKit installer

--- scripts/mottekit/install.sh
+++ scripts/mottekit/install.sh
@@ -7,7 +7,9 @@ readonly mottekit_dir=~/.mottekit
 [ -e "$mottekit_dir" ] &&
     { echo "The $mottekit_dir directory already exists" >&2; exit 1; }
 
-for i in ~/.local/bin ~/bin; do
+path_dirs=(~/.local/bin ~/bin)
+
+for i in "${path_dirs[@]}"; do
     [ "$PATH" = "$i" ] || [[ "$PATH" = *:"$i":* ]] ||
         [[ "$PATH" = "$i":* ]] || [[ "$PATH" = *:"$i" ]] &&
         { entrypoint=$i/mottekit; break; }
@@ -15,12 +17,17 @@ done
 
 [ -z "$entrypoint" ] && {
     echo 'Cannot find a location in PATH suitable for installing the' \
-        'MotteKit entrypoint' >&2
+        'MotteKit entrypoint. You must have one of the following' \
+        'directories in your PATH:' >&2
+    echo "${path_dirs[@]@Q}" >&2
     exit 1
 }
 
 [ -e "$entrypoint" ] &&
     { echo "The entrypoint $entrypoint already exists" >&2; exit 1; }
+
+command -v git >/dev/null ||
+    { echo 'The git command is required but cannot be found' >&2; exit 1; }
 
 echo "Creating the MotteKit directory $mottekit_dir"
 mkdir "$mottekit_dir"
