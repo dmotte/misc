1256059bd131daf0091fea1928b8c91bb7c0a740
dmotte <37443982+dmotte@users.noreply.github.com>
2025-03-01 15:47:38 +0100

    Reworked standalone-vscode.sh

--- scripts/provisioning/standalone-vscode.sh
+++ scripts/provisioning/standalone-vscode.sh
@@ -2,74 +2,127 @@
 
 set -e
 
-# TODO make this similar to the other provisioning scripts
-# TODO add support for data directory
-# TODO option --launcher=default or --launcher=/path/to/vscode.desktop or --launcher= (default, no launcher is created)
-
 # This script can be used to set up a standalone installation of Visual Studio
 # Code in a specific directory
 
 # Tested on Debian 12 (bookworm)
 
 # To run this script without downloading it:
-# bash <(curl -fsSL https://raw.githubusercontent.com/dmotte/misc/main/scripts/provisioning/standalone-vscode.sh) linux-x64
+# bash <(curl -fsSL https://raw.githubusercontent.com/dmotte/misc/main/scripts/provisioning/standalone-vscode.sh) -ulauto
 
-# Ensure that some variables are defined
-: "${1:?}"
+options=$(getopt -o +b:o:a:c:d:uDl: -l build: -l os: -l arch: -l checksum: \
+    -l install-dir: -l update -l create-data -l launcher: -- "$@")
+eval "set -- $options"
 
-readonly main_dir=${STANDALONE_VSCODE_MAIN_DIR:-$HOME/apps/vscode}
-readonly archive_url="https://code.visualstudio.com/sha/download?build=stable&os=$1"
-readonly archive_path=$main_dir/archive.tar.gz
+build=stable
+os=''
+arch=x64
+checksum=''
+install_dir="$HOME/apps/vscode"
+update=n
+create_data=n
+launcher=''
 
-readonly launcher_default=~/.local/share/applications/vscode.desktop
-if [ -n "$STANDALONE_VSCODE_LAUNCHER" ]; then
-    launcher=$STANDALONE_VSCODE_LAUNCHER
-elif [ -d "$(dirname "$launcher_default")" ]; then
-    launcher=$launcher_default
+while :; do
+    case $1 in
+        -b|--build) shift; build=$1;;
+        -o|--os) shift; os=$1;;
+        -a|--arch) shift; arch=$1;;
+        -c|--checksum) shift; checksum=$1;;
+        -d|--install-dir) shift; install_dir=$1;;
+        -u|--update) update=y;;
+        -D|--create-data) create_data=y;;
+        -l|--launcher) shift; launcher=$1;;
+        --) shift; break;;
+    esac
+    shift
+done
+
+if [ -z "$os" ]; then
+    if [[ "$(uname)" = MINGW* ]]
+        then os=win32
+        else os=linux
+    fi
 fi
+
+[ "$launcher" != auto ] || launcher=~/.local/share/applications/vscode.desktop
 
 ################################################################################
 
-if [ -d "$main_dir" ]; then
-    echo "Directory $main_dir already exists. Installed app version:"
-    "$main_dir/VSCode-$1/bin/code" -v
+if [ "$update" = y ]; then
+    if [ -d "$install_dir" ]; then
+        echo "Directory $install_dir already exists. Installed app version:"
+        "$install_dir/vscode/bin/code" -v
 
-    main_dir_old=$main_dir-old-$(date -u +%Y-%m-%d-%H%M%S)
-    echo "Moving $main_dir to $main_dir_old"
-    mv -T "$main_dir" "$main_dir_old"
+        install_dir_old=$install_dir-old-$(date -u +%Y-%m-%d-%H%M%S)
+        echo "Moving $install_dir to $install_dir_old"
+        mv -T "$install_dir" "$install_dir_old"
+    else
+        echo "Directory $install_dir doesn't already exists. Installing from" \
+            'scratch'
+    fi
+elif [ -d "$install_dir" ]; then
+    echo "Directory $install_dir already exists" >&2; exit 1
 fi
 
-mkdir -p "$main_dir"
+mkdir -p "$install_dir"
+
+if [ "$os" = win32 ]; then
+    readonly archive_url="https://code.visualstudio.com/sha/download?build=$build&os=$os-$arch-archive"
+    readonly archive_path=$install_dir/archive.zip
+else
+    readonly archive_url="https://code.visualstudio.com/sha/download?build=$build&os=$os-$arch"
+    readonly archive_path=$install_dir/archive.tar.gz
+fi
 
 echo "Downloading $archive_url to $archive_path"
 curl -fLo "$archive_path" "$archive_url"
 
-echo "Extracting $archive_path to $main_dir"
-tar -xzf "$archive_path" -C "$main_dir"
+if [ -n "$checksum" ]; then
+    echo "$checksum $archive_path" | sha256sum -c
+fi
 
-readonly data_old=$main_dir_old/VSCode-$1/data
-readonly data_new=$main_dir/VSCode-$1/data
-if [ -n "$main_dir_old" ] && [ -d "$data_old" ]; then
-    echo "Moving data dir from $data_old to $data_new"
-    mv -T "$data_old" "$data_new"
+echo "Extracting $archive_path to $install_dir"
+if [ "$os" = win32 ]; then
+    unzip -q "$archive_path" -d "$install_dir/vscode"
+else
+    tar -xzf "$archive_path" -C "$install_dir"
+    mv -T "$install_dir/VSCode-$os-$arch" "$install_dir/vscode"
+fi
+
+readonly data_dir=$install_dir/vscode/data
+
+if [ "$update" = y ] && [ -n "$install_dir_old" ]; then
+    readonly data_old=$install_dir_old/vscode/data
+
+    if [ -d "$data_old" ]; then
+        echo "Moving data dir from $data_old to $data_dir"
+        mv -T "$data_old" "$data_dir"
+    else
+        echo 'Not moving data dir from the old installation because it does' \
+            'not exist'
+    fi
+fi
+
+# The data directory enables Visual Studio Code's Portable mode. See
+# https://code.visualstudio.com/docs/editor/portable
+if [ "$create_data" = y ] && [ ! -d "$data_dir" ]; then
+    echo "Creating data dir $data_dir"
+    mkdir "$data_dir"
 fi
 
 echo 'Installed app version:'
-"$main_dir/VSCode-$1/bin/code" -v
-
-################################################################################
+"$install_dir/vscode/bin/code" -v
 
 if [ -n "$launcher" ]; then
-    echo "Creating launcher file in $launcher"
+    echo "Creating launcher file $launcher"
     install -m644 /dev/stdin "$launcher" << EOF
 [Desktop Entry]
 Name=Visual Studio Code
-Exec=$main_dir/VSCode-$1/code %f
+Exec=$install_dir/vscode/code %f
 Comment=Visual Studio Code
 Terminal=false
-Icon=$main_dir/VSCode-$1/resources/app/resources/linux/code.png
+Icon=$install_dir/vscode/resources/app/resources/linux/code.png
 Type=Application
 EOF
 fi
-
-echo 'Installation completed successfully'
