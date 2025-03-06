509560bba7b7f4f45880faadcbade3740a130706
dmotte <37443982+dmotte@users.noreply.github.com>
2025-03-06 03:21:12 +0100

    Added winemono.sh

---
+++ scripts/provisioning/winemono.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+# This script downloads the Wine Mono installer to the Wine cache of the
+# current user. This can be useful because, if the installer is found in the
+# cache when a wineprefix is created, Wine will skip the download prompt and
+# silently install Wine Mono automatically.
+
+# Tested on Debian 12 (bookworm)
+
+# Usage example:
+#   ./winemono.sh && wineboot --init
+
+readonly wine_cache=~/.cache/wine
+
+wine_version=$(wine --version)
+
+case $wine_version in
+wine-10.0)
+    # Source: https://gitlab.winehq.org/wine/wine/-/blob/wine-10.0/dlls/appwiz.cpl/addons.c?ref_type=tags#L59
+    readonly installer_url='http://source.winehq.org/winemono.php?arch=x86_64&v=9.4.0&winev=10.0'
+    readonly installer_checksum='cf6173ae94b79e9de13d9a74cdb2560a886fc3d271f9489acb1cfdbd961cacb2'
+    readonly installer_path="$wine_cache/wine-mono-9.4.0-x86.msi"
+    ;;
+*)
+    echo "Unsupported Wine version $wine_version" >&2
+    exit 1
+    ;;
+esac
+
+tmpdir=$(mktemp -d --tmpdir winemono-XXXXXXXXXX)
+trap 'rm -rf $tmpdir' EXIT
+
+readonly tmp_installer=$tmpdir/tmp-installer
+
+echo "Downloading Wine Mono installer $installer_url to $tmp_installer"
+curl -fLo "$tmp_installer" "$installer_url"
+
+echo "Verifying checksum for $tmp_installer"
+echo "$installer_checksum" "$tmp_installer" | sha256sum -c
+
+install -DTm644 "$tmp_installer" "$installer_path"
