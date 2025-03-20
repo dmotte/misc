a4ad8a6630d75c52827198f02a48403020c849b6
dmotte <37443982+dmotte@users.noreply.github.com>
2025-03-20 23:31:25 +0100

    Added gh-pr-approve bookmarklet

---
+++ bookmarklets/gh-pr-approve.js
@@ -0,0 +1,30 @@
+// Bookmarklet to instantly approve a GitHub PR (Pull Request) even if it's
+// merged
+
+// Tested on https://github.com/.../.../pull/.../files on 2025-03-20 with
+// Google Chrome version 130.0.6723.59 (Official Build) (64-bit)
+
+// javascript:(function(){
+
+function querySelectorOrErr(selectors) {
+  const result = document.querySelector(selectors);
+  if (result === null) throw new Error(`Element ${selectors} not found`);
+  return result;
+}
+
+try {
+  const inputReviewEvent = querySelectorOrErr(
+    "input[type='hidden'][name='pull_request_review[event]']"
+  );
+  const formSubmitReview = querySelectorOrErr(
+    "form#pull_requests_submit_review"
+  );
+
+  inputReviewEvent.value = "approve";
+
+  formSubmitReview.submit();
+} catch (error) {
+  alert("Error: " + error);
+}
+
+// })();
