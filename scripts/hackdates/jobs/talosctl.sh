638c30f2f057a1b516ed4deb71bb0c1131dcc512
dmotte <37443982+dmotte@users.noreply.github.com>
2025-03-29 16:38:20 +0100

    Added Hackdates talosctl job

---
+++ scripts/hackdates/jobs/talosctl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+v_local=$(talosctl version --client --short | sed -En 's/^Client (.+)$/\1/p')
+
+v_latest=$(curl -fsSL https://api.github.com/repos/siderolabs/talos/releases/latest |
+    sed -En 's/^  "name": "([^"]+)",$/\1/p')
+
+[ "$v_local" = "$v_latest" ] || { echo "Version mismatch" >&2; exit 1; }
