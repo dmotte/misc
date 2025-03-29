0b465220c273aa4dd72dc55133d1aa78c63933ed
dmotte <37443982+dmotte@users.noreply.github.com>
2025-03-29 16:18:21 +0100

    Added Hackdates msgbuf job

---
+++ scripts/hackdates/jobs/msgbuf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+v_local=$(echo -n v; msgbuf --version | cut -d' ' -f2)
+
+v_latest=$(curl -fsSL https://api.github.com/repos/dmotte/msgbuf/releases/latest |
+    sed -En 's/^  "name": "([^"]+)",$/\1/p')
+
+[ "$v_local" = "$v_latest" ] || { echo "Version mismatch" >&2; exit 1; }
