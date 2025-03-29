2c926e3166b9e58f12275d48f7af4798de34faf4
dmotte <37443982+dmotte@users.noreply.github.com>
2025-03-29 15:01:30 +0100

    Added code for helm Hackdates job

--- scripts/hackdates/jobs/helm.sh
+++ scripts/hackdates/jobs/helm.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-echo TODO
+v_local=$(helm version --template='{{.Version}}')
+
+v_latest=$(curl -fsSL https://api.github.com/repos/helm/helm/releases/latest |
+    sed -En 's/^  "tag_name": "([^"]+)",$/\1/p')
+
+[ "$v_local" = "$v_latest" ] || { echo "Version mismatch" >&2; exit 1; }
