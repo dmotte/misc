b6f53a50dc5116f9ca52790e99b4274876e01ff6
dmotte <37443982+dmotte@users.noreply.github.com>
2025-03-29 14:41:56 +0100

    Simplified kubectl Hackdates job

--- scripts/hackdates/jobs/kubectl.sh
+++ scripts/hackdates/jobs/kubectl.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-job_name=$(basename "$0")
-job_name=${job_name%.sh}
-
-v_local=$(kubectl version --client)
-v_local=$(echo "$v_local" | sed -En 's/^Client Version: (.+)$/\1/p')
-echo "$job_name v_local: $v_local"
+v_local=$(kubectl version --client | sed -En 's/^Client Version: (.+)$/\1/p')
 
 v_latest=$(curl -fsSL https://dl.k8s.io/release/stable.txt)
-echo "$job_name v_latest: $v_latest"
 
-if [ "$v_local" = "$v_latest" ]
-    then echo "$job_name: versions match"
-    else echo "$job_name: versions do NOT match" >&2; exit 1
-fi
+[ "$v_local" = "$v_latest" ] || { echo "Version mismatch" >&2; exit 1; }
