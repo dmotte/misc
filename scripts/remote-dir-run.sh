45a0dfebd2a0dbaad6427b73001291f81de0686e
dmotte <37443982+dmotte@users.noreply.github.com>
2025-05-30 14:19:48 +0200

    Simplified remote-dir-run arg 1

--- scripts/remote-dir-run.sh
+++ scripts/remote-dir-run.sh
@@ -16,8 +16,7 @@ set -e
 # For debugging purposes:
 #   RDR_DEBUG=true RDR_EVAL=exit ./remote-dir-run.sh mydir foo ++ bar baz
 
-[ $# -ge 2 ] || { echo 'Not enough args' >&2; exit 1; }
-readonly local_dir=$1; shift
+readonly local_dir=${1:?}; shift
 
 [ -e "$local_dir/main.sh" ] || { echo 'File main.sh not found' >&2; exit 1; }
 
