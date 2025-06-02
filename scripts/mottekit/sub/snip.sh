ef2b3058251abd91fab2f1f9a4305844e65b040f
dmotte <37443982+dmotte@users.noreply.github.com>
2025-06-02 22:45:07 +0200

    MotteKit snip support for empty string

--- scripts/mottekit/sub/snip.sh
+++ scripts/mottekit/sub/snip.sh
@@ -4,8 +4,11 @@ set -e
 
 cd "$(dirname "$0")"
 
-readonly text=${1:?}
+readonly text=$1
 
 reporoot=$(git rev-parse --show-toplevel)
+readonly snippets_file=$reporoot/snippets/README.md
 
-exec grep -Fi --color=auto "$text" "$reporoot/snippets/README.md"
+[ -n "$text" ] || exec cat "$snippets_file"
+
+exec grep -Fi --color=auto "$text" "$snippets_file"
