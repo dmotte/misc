a0265d15ba84b1902484d3a906eb3a96410f0868
dmotte <37443982+dmotte@users.noreply.github.com>
2025-06-15 02:28:48 +0200

    Added git diff check to awesome/cicd.sh

--- awesome/cicd.sh
+++ awesome/cicd.sh
@@ -6,5 +6,10 @@ set -e
 
 cd "$(dirname "$0")"
 
+git diff --quiet HEAD^ HEAD -- . && {
+    echo "Skipping $0 as there are no changes in $PWD in the latest commit"
+    exit
+}
+
 # Tested with Python 3.12.4 on Windows 10
 OUTPUT_DATA=false exec python3 parse.py < README.md
