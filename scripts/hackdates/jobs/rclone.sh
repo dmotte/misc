c67e2c4d875b6bb2e901ae43c5a2a672cf701820
dmotte <37443982+dmotte@users.noreply.github.com>
2025-07-05 17:19:19 +0200

    Hackdates added rclone.sh

---
+++ scripts/hackdates/jobs/rclone.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+v_local=$(rclone version | head -n1)
+
+v_latest=$(curl -fsSL https://api.github.com/repos/rclone/rclone/releases/latest |
+    sed -En 's/^  "name": "([^"]+)",$/\1/p')
+
+[ "$v_local" = "$v_latest" ] || { echo "Version mismatch" >&2; exit 1; }
