6538b3ecba11cd45967bee0861e40f4bdcb0222e
dmotte <37443982+dmotte@users.noreply.github.com>
2025-08-23 17:31:19 +0200

    Added vlc-detached script

---
+++ scripts/vlc-detached.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+# This script runs VLC detached from the terminal window
+
+readonly vlc_win=/c/Program Files/VideoLAN/VLC/vlc.exe
+
+if command -v vlc >/dev/null; then
+    vlc=vlc
+elif [[ "$(uname)" = MINGW* ]] && [ -e "$vlc_win" ]; then
+    vlc=$vlc_win
+else
+    echo 'VLC not found' >&2; exit 1
+fi
+
+nohup "$vlc" "$@" >/dev/null 2>&1 & disown
