1bc47c24d671686b18bf31fcfdc5f4538f9dfb19
dmotte <37443982+dmotte@users.noreply.github.com>
2025-09-24 14:37:27 +0200

    Usage examples for vlc-detached.sh

--- scripts/vlc-detached.sh
+++ scripts/vlc-detached.sh
@@ -4,6 +4,12 @@ set -e
 
 # This script runs VLC detached from the terminal window
 
+# Usage example:
+#   bash vlc-detached.sh ~/mymusic/*.mp3
+
+# Usage example useful for Git Bash on Windows:
+#   cygpath -w ~/mymusic/*.mp3 | xargs -rd\\n bash vlc-detached.sh
+
 readonly vlc_win='/c/Program Files/VideoLAN/VLC/vlc.exe'
 
 if command -v vlc >/dev/null; then
