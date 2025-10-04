483b22f58ccd3f8d3a7709f229daee6839f184a8
dmotte <37443982+dmotte@users.noreply.github.com>
2025-10-04 04:17:10 +0200

    Added add_grep_args to check-no-trailing-spaces.sh

--- scripts/cicd/check-no-trailing-spaces.sh
+++ scripts/cicd/check-no-trailing-spaces.sh
@@ -3,7 +3,8 @@
 set -e
 
 readonly main_dir=${1:?}; shift
+readonly add_grep_args=("$@")
 
-if grep -IRl --exclude-dir=.git '\s$' "$main_dir"; then
+if grep -IRl --exclude-dir=.git "${add_grep_args[@]}" '\s$' "$main_dir"; then
     echo 'Trailing spaces found in some files' >&2; exit 1
 fi
