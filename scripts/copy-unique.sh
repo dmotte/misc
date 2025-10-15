0965479c3f7afc643d9f979067961ba9bb7cfbe9
dmotte <37443982+dmotte@users.noreply.github.com>
2025-10-15 14:23:06 +0200

    Fixed copy-unique.sh a bit

--- scripts/copy-unique.sh
+++ scripts/copy-unique.sh
@@ -5,7 +5,7 @@ set -e
 # This script copies a file, but only if the destination directory doesn't
 # contain a file with the same SHA-256 checksum
 
-# Usage example: ./copy-unique.sh myfile.txt mydir/bak-myfile-$(date +%Y-%m-%d-%H%M%S).txt
+# Usage example: ./copy-unique.sh myfile.txt "mydir/bak-myfile-$(date +%Y-%m-%d-%H%M%S).txt"
 
 readonly file_src=${1:?} file_dst=${2:?}
 
@@ -20,7 +20,7 @@ pair_src=$(sha256sum "$file_src")
 checksum_src=$(echo "$pair_src" | cut -d' ' -f1)
 
 find_files_dst=$(find "$dir_dst" -mindepth 1 -maxdepth 1 -type f)
-pairs_dst=$(echo "$find_files_dst" | xargs -rd\\n sha256sum)
+pairs_dst=$(echo -n "$find_files_dst" | xargs -rd\\n sha256sum)
 
 if echo "$pairs_dst" | grep -i "^$checksum_src "; then
     echo 'A file with the same SHA-256 checksum is already present in' \
