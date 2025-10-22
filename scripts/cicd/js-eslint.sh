83a95b08f109d75ecf44b4531e1a8ab8ff071dd1
dmotte <37443982+dmotte@users.noreply.github.com>
2025-10-22 17:59:13 +0200

    Updated nvm to v0.40.3

--- scripts/cicd/js-eslint.sh
+++ scripts/cicd/js-eslint.sh
@@ -19,8 +19,8 @@ export default [
 echo "::group::$0: Preparation"
     if ! command -v npm; then
         bash <(fetch_and_check \
-            'https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh' \
-            '69da4f89f430cd5d6e591c2ccfa2e9e3ad55564ba60f651f00da85e04010c640')
+            'https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh' \
+            '2d8359a64a3cb07c02389ad88ceecd43f2fa469c06104f92f98df5b6f315275f')
         # shellcheck source=/dev/null
         . ~/.nvm/nvm.sh
         nvm install --lts
