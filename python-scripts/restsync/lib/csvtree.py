1d397ff746a523d7455a6806a08bef7f43bc4ff0
dmotte <37443982+dmotte@users.noreply.github.com>
2025-11-13 17:41:21 +0100

    Restsync added mode to trees

--- python-scripts/restsync/lib/csvtree.py
+++ python-scripts/restsync/lib/csvtree.py
@@ -16,19 +16,20 @@ def tree_snapshot(rinv: ResticInvoker, snapshot_id: str) -> Iterator[str]:
     Builds the CSV tree of the contents of a restic snapshot
     '''
     for entry in rinv.restic_json(['ls', snapshot_id]):
-        if not all(k in entry for k in ('type', 'path', 'mtime')):
+        if not all(k in entry for k in ('type', 'path', 'mode', 'mtime')):
             continue
 
         entry['path'] = entry['path'].removeprefix('/')
+        entry['mode'] = oct(stat.S_IMODE(entry['mode']))[2:]
 
         if entry['type'] == 'dir':
-            yield f'{entry['path']}/;-1;DIR\n'
+            yield f'{entry['path']}/;{entry['mode']};-1;DIR\n'
             continue
 
         entry['size'] = str(entry['size'])
         entry['mtime'] = str(int(dt.fromisoformat(entry['mtime']).timestamp()))
 
-        yield f'{entry['path']};{entry['size']};{entry['mtime']}\n'
+        yield f'{entry['path']};{entry['mode']};{entry['size']};{entry['mtime']}\n'
 
 
 def tree_local(root_path: str, excludes: list[str] = [],
@@ -47,12 +48,14 @@ def tree_local(root_path: str, excludes: list[str] = [],
 
         st = entry.stat(follow_symlinks=False)
 
+        mode_str = oct(stat.S_IMODE(st.st_mode))[2:]
+
         if stat.S_ISDIR(st.st_mode):
-            yield f'{rel_path}/;-1;DIR\n'
+            yield f'{rel_path}/;{mode_str};-1;DIR\n'
             yield from tree_local(os.path.join(root_path, entry.name),
                                   excludes, f'{rel_path}/')
         else:
-            yield f'{rel_path};{st.st_size};{int(st.st_mtime)}\n'
+            yield f'{rel_path};{mode_str};{st.st_size};{int(st.st_mtime)}\n'
 
 
 def trees_equal(tree_a: Iterator[str], tree_b: Iterator[str]) -> bool:
