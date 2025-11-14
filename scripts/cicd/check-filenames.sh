d6e3a554f556062620c28afac3c2bc9155600d5c
dmotte <37443982+dmotte@users.noreply.github.com>
2025-11-14 03:00:36 +0100

    Escaped exclamation marks in find cmds

--- scripts/cicd/check-filenames.sh
+++ scripts/cicd/check-filenames.sh
@@ -5,7 +5,7 @@ set -e
 readonly main_dir=${1:?}; shift
 readonly grep_args=("$@")
 
-filenames=$(find "$main_dir" ! -name .git ! -path '*/.git/*' -printf '%f\n')
+filenames=$(find "$main_dir" \! -name .git \! -path '*/.git/*' -printf '%f\n')
 
 if echo "$filenames" | grep "${grep_args[@]}"; then
     echo 'Pattern found in some filenames' >&2; exit 1
