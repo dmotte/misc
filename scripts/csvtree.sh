90aa00ef1f43226afd0cbfd704619ccfba352b93
dmotte <37443982+dmotte@users.noreply.github.com>
2025-11-15 17:46:51 +0100

    Moved -mindepth 1 to usage example in csvtree.sh

--- scripts/csvtree.sh
+++ scripts/csvtree.sh
@@ -10,12 +10,12 @@ set -e
 # Note: the output should be similar to "rclone lsf -R --format=pst"
 
 # Usage example:
-#   bash csvtree.sh mydir \! -name myexclude.txt > tree-mydir.csv
+#   bash csvtree.sh mydir -mindepth 1 \! -name myexclude.txt > tree-mydir.csv
 
 readonly main_dir=${1:?}; shift
 readonly add_find_args=("$@")
 
-tree=$(find "$main_dir" -mindepth 1 "${add_find_args[@]}" \( \
+tree=$(find "$main_dir" "${add_find_args[@]}" \( \
     \( -type d -printf '%P/;%#m;-1;DIR\n' \) \
     -o \( -type f -printf '%P;%#m;%s;%T@\n' \) \
 \))
