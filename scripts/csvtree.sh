682449d6a8b437ac541f21c801018dc9e723391e
dmotte <37443982+dmotte@users.noreply.github.com>
2025-11-15 17:29:16 +0100

    Added csvtree.sh script

---
+++ scripts/csvtree.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+# This script can be used to generate a CSV tree of a specific directory
+
+# Note: the semicolon character (";") is used as field separator, as it is
+# normally not allowed in filenames
+
+# Note: the output should be similar to "rclone lsf -R --format=pst"
+
+readonly main_dir=${1:?}; shift
+readonly add_find_args=("$@")
+
+tree=$(find "$main_dir" -mindepth 1 "${add_find_args[@]}" \( \
+    \( -type d -printf '%P/;%#m;-1;DIR\n' \) \
+    -o \( -type f -printf '%P;%#m;%s;%T@\n' \) \
+\))
+
+echo "$tree" | LC_ALL=C sort -t\; -k1,1
