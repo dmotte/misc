dd882af0fa5ec189241d96b530f424920e9c522f
dmotte <37443982+dmotte@users.noreply.github.com>
2026-01-12 19:13:00 +0100

    Reworked hackdates/jobs/msgbuf.sh to avoid set -x

--- scripts/hackdates/jobs/msgbuf.sh
+++ scripts/hackdates/jobs/msgbuf.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-v_local=$(echo -n v; msgbuf --version | cut -d' ' -f2)
+echo 'Checking msgbuf version'
 
-v_latest=$(curl -fsSL https://api.github.com/repos/dmotte/msgbuf/releases/latest |
-    sed -En 's/^  "name": "([^"]+)",$/\1/p')
+text=$(echo -n v; msgbuf --version)
+v_local=$(echo "$text" | cut -d' ' -f2)
 
-[ "$v_local" = "$v_latest" ] || { echo 'Version mismatch' >&2; exit 1; }
+text=$(curl -fsSL https://api.github.com/repos/dmotte/msgbuf/releases/latest)
+v_latest=$(echo "$text" | sed -En 's/^  "name": "([^"]+)",$/\1/p')
+
+if [ "$v_local" = "$v_latest" ]
+    then echo "OK ($v_local)"
+    else echo "ERROR: local is $v_local but latest is $v_latest" >&2; exit 1
+fi
