d0608b6aa36c2b6c89126b1d55e524605dc9811e
dmotte <37443982+dmotte@users.noreply.github.com>
2026-01-13 00:58:38 +0100

    Fixed hackdates/jobs/rpi-imager.sh to work with v2.x

--- scripts/hackdates/jobs/rpi-imager.sh
+++ scripts/hackdates/jobs/rpi-imager.sh
@@ -5,7 +5,7 @@ set -e
 echo 'Checking Raspberry Pi Imager (rpi-imager) version'
 
 text=$(rpi-imager --version 2>&1)
-v_local=$(echo "$text" | sed -En 's/^rpi-imager version (.+)$/v\1/p')
+v_local=$(echo "$text" | sed -En 's/^Raspberry Pi Imager (v.+)$/\1/p')
 
 text=$(curl -fsSL https://api.github.com/repos/raspberrypi/rpi-imager/releases/latest)
 v_latest=$(echo "$text" | sed -En 's/^  "tag_name": "([^"]+)",$/\1/p')
