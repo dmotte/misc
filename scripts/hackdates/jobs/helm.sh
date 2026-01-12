4d1d6e280890a454e3ede4214949f752b5de40b8
dmotte <37443982+dmotte@users.noreply.github.com>
2026-01-12 18:48:30 +0100

    Reworked hackdates/jobs/helm.sh to avoid set -x

--- scripts/hackdates/jobs/helm.sh
+++ scripts/hackdates/jobs/helm.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-set -ex
+set -e
+
+echo 'Checking Helm version'
 
 v_local=$(helm version --template='{{.Version}}')
 
-v_latest=$(curl -fsSL https://api.github.com/repos/helm/helm/releases/latest |
-    sed -En 's/^  "tag_name": "([^"]+)",$/\1/p')
+text=$(curl -fsSL https://api.github.com/repos/helm/helm/releases/latest)
+v_latest=$(echo "$text" | sed -En 's/^  "tag_name": "([^"]+)",$/\1/p')
 
-[ "$v_local" = "$v_latest" ] || { echo 'Version mismatch' >&2; exit 1; }
+if [ "$v_local" = "$v_latest" ]
+    then echo "OK ($v_local)"
+    else echo "ERROR: local is $v_local but latest is $v_latest" >&2; exit 1
+fi
