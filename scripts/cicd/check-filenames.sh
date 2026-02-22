a7d17c78ea44929513fdacc48af649575cfcba8c
dmotte <37443982+dmotte@users.noreply.github.com>
2026-02-22 22:40:56 +0100

    feat(check-filenames): add support for USE_GIT_LS_FILES env var

--- scripts/cicd/check-filenames.sh
+++ scripts/cicd/check-filenames.sh
@@ -5,7 +5,11 @@ set -e
 readonly main_dir=${1:?}; shift
 readonly grep_args=("$@")
 
-filenames=$(find "$main_dir" \! -name .git \! -path '*/.git/*' -printf '%f\n')
+if [ "$USE_GIT_LS_FILES" = true ]
+    then filenames=$(git -C "$main_dir" ls-files)
+    else filenames=$(find "$main_dir" \
+        \! -name .git \! -path '*/.git/*' -printf '%f\n')
+fi
 
 if echo "$filenames" | grep "${grep_args[@]}"; then
     echo 'Pattern found in some filenames' >&2; exit 1
