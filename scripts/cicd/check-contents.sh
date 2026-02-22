a701d5b2897d9ed168d5d13d25b8d9b52be39bd1
dmotte <37443982+dmotte@users.noreply.github.com>
2026-02-22 22:28:32 +0100

    feat(check-contents): add support for USE_GIT_GREP env var

--- scripts/cicd/check-contents.sh
+++ scripts/cicd/check-contents.sh
@@ -5,6 +5,10 @@ set -e
 readonly main_dir=${1:?}; shift
 readonly add_grep_args=("$@")
 
-if grep -IRl --exclude-dir=.git "${add_grep_args[@]}" "$main_dir"; then
-    echo 'Pattern found in some files' >&2; exit 1
-fi
+result=0
+if [ "$USE_GIT_GREP" = true ]
+    then git -C "$main_dir" grep -Il "${add_grep_args[@]}"
+    else grep -IRl --exclude-dir=.git "${add_grep_args[@]}" "$main_dir"
+fi || result=$?
+
+[ "$result" != 0 ] || { echo 'Pattern found in some files' >&2; exit 1; }
