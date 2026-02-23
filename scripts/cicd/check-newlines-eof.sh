344294189d8cb9d080aa192d22861e7dffb9cc60
dmotte <37443982+dmotte@users.noreply.github.com>
2026-02-23 02:50:52 +0100

    feat(check-newlines-eof): add support for USE_GIT_LS_FILES env var

--- scripts/cicd/check-newlines-eof.sh
+++ scripts/cicd/check-newlines-eof.sh
@@ -3,10 +3,23 @@
 set -e
 
 readonly main_dir=${1:?}; shift
-readonly add_find_args=("$@")
+readonly add_ls_args=("$@")
 
-files=$(find "$main_dir" -type f \! -path '*/.git/*' "${add_find_args[@]}")
-files_bad=$(echo -n "$files" | xargs -rd\\n grep -ILPz '\n\z' || :)
+if [ "$USE_GIT_LS_FILES" = true ]; then
+    files=$(git -C "$main_dir" ls-files "${add_ls_args[@]}")
+    files=$(echo "$files" | while IFS= read -r i; do echo "$main_dir/$i"; done)
+else
+    files=$(find "$main_dir" -type f \! -path '*/.git/*' "${add_ls_args[@]}")
+fi
+
+# We don't use grep's "-I" option here because, since we also use "-z",
+# grep is expecting NUL bytes, so its binary detection becomes
+# unreliable
+files_bad=$(echo -n "$files" | xargs -rd\\n grep -LPz '\n\z' || :)
+
+# Filter out binary files
+files_bad=$(echo "$files_bad" | while IFS= read -r i; do
+    grep -I . "$i" >/dev/null || continue; echo "$i"; done)
 
 [ -z "$files_bad" ] || {
     echo "$files_bad"
