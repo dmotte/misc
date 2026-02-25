96939a4abee3cc1701d58b243a8a98079135fac4
dmotte <37443982+dmotte@users.noreply.github.com>
2026-02-25 20:07:53 +0100

    feat(check-readme-headers): add support for USE_GIT_LS_FILES env var

--- scripts/cicd/check-readme-headers.sh
+++ scripts/cicd/check-readme-headers.sh
@@ -7,9 +7,17 @@ readonly main_dir=${1:?}; shift
 # We need to work with absolute paths, otherwise this script would fail in
 # case a "./README.md" file exists
 realpath_main_dir=$(realpath "$main_dir")
-readmes=$(find "$realpath_main_dir" -type f -iname README.md)
 
-echo "$readmes" | while IFS= read -r i; do
+if [ "$USE_GIT_LS_FILES" = true ]; then
+    files=$(git -C "$main_dir" ls-files \
+        ':(icase)README.md' ':(icase)*/README.md')
+    files=$(echo "$files" | while IFS= read -r i; do
+        echo "$realpath_main_dir/$i"; done)
+else
+    files=$(find "$realpath_main_dir" -type f -iname README.md)
+fi
+
+echo "$files" | while IFS= read -r i; do
     echo "Checking $i"
 
     parent_dir=${i%/*}
