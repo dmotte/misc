6ed098d21daf147ae01d65fc6708883eae598bf9
dmotte <37443982+dmotte@users.noreply.github.com>
2026-02-25 19:27:40 +0100

    feat(check-no-trailing-spaces): add support for USE_GIT_GREP env var

--- scripts/cicd/check-no-trailing-spaces.sh
+++ scripts/cicd/check-no-trailing-spaces.sh
@@ -5,6 +5,10 @@ set -e
 readonly main_dir=${1:?}; shift
 readonly add_grep_args=("$@")
 
-if grep -IRl --exclude-dir=.git "${add_grep_args[@]}" '\s$' "$main_dir"; then
-    echo 'Trailing spaces found in some files' >&2; exit 1
-fi
+result=0
+if [ "$USE_GIT_GREP" = true ]
+    then git -C "$main_dir" grep -Il '\s$' "${add_grep_args[@]}"
+    else grep -IRl --exclude-dir=.git "${add_grep_args[@]}" '\s$' "$main_dir"
+fi || result=$?
+
+[ "$result" != 0 ] || { echo 'Trailing spaces found in some files' >&2; exit 1; }
