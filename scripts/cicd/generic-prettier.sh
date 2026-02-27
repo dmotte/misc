517b6dd8651d7d1fa56f3fe5d4c1fc49cf8bc87d
dmotte <37443982+dmotte@users.noreply.github.com>
2026-02-27 13:32:59 +0100

    feat(generic-prettier): support for nested ignore files

--- scripts/cicd/generic-prettier.sh
+++ scripts/cicd/generic-prettier.sh
@@ -20,4 +20,58 @@ echo "::group::$0: Preparation"
     npx prettier --version
 echo '::endgroup::'
 
-npx prettier -c .
+[ "$NESTED_IGNORE_FILES" = true ] || exec npx prettier -c .
+
+# We needed to implement this custom logic because Prettier doesn't consider
+# ignore files in subdirectories. See
+# https://github.com/prettier/prettier/issues/4081 for more info.
+# We have to actually rewrite all the patterns and we cannot simply rely on
+# passing multiple "--ignore-path" options to Prettier because, in such case,
+# if a pattern like "*.json" is present in ANY of the ignore files at any
+# depth, then Prettier would ignore all such files AT ANY DEPTH, even outside
+# the directory where the ignore file was found
+
+if [ "$USE_GIT_LS_FILES" = true ]
+    then ignfiles=$(git ls-files '.gitignore' '*/.gitignore' \
+        '.prettierignore' '*/.prettierignore')
+    else ignfiles=$(find . -type f \
+        \( -name .gitignore -o -name .prettierignore \) -printf '%P\n')
+fi
+
+readonly combfile=.combined.prettierignore
+[ ! -e "$combfile" ] || { echo "File $combfile already exists" >&2; exit 1; }
+:> "$combfile" # Empty file
+trap 'rm -v $combfile' EXIT
+
+while IFS= read -r ignfile; do
+    dn=${ignfile%/*}; [ "$dn" != "$ignfile" ] || dn=''
+
+    # We use "echo $(<...)" here to remove all trailing newlines
+    [ -n "$dn" ] || { echo "$(<"$ignfile")" >> "$combfile"; continue; }
+
+    while IFS= read -r line || [ -n "$line" ]; do
+        if [ -z "$line" ] || [[ "$line" = \#* ]]; then continue; fi
+
+        # Rewrite the pattern based on official gitignore specification
+        # https://git-scm.com/docs/gitignore#_pattern_format
+
+        prefix=''; pattern=$line
+
+        [[ "$pattern" != \!* ]] || { prefix=\!; pattern=${pattern:1}; }
+
+        if [[ "$pattern" = /* ]]; then
+            echo "$prefix$dn$pattern" >> "$combfile"
+        elif [[ "$pattern" =~ /[^/] ]]; then
+            echo "$prefix$dn/$pattern" >> "$combfile"
+        else
+            echo "$prefix$dn/**/$pattern" >> "$combfile"
+        fi
+    done < "$ignfile"
+done < <(echo "$ignfiles")
+
+# We cannot use "exec" here because we want to run the EXIT trap before
+# exiting.
+# We cannot use "--ignore-path=<(echo ...)" here because Prettier evaluates
+# ignore files relatively to their path. That's a reason why we need a
+# dedicated "$combfile" stored in proper location
+npx prettier --ignore-path="$combfile" -c .
