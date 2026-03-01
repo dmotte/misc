12a0994a1b4a73d28578aaf10148d86acf226685
dmotte <37443982+dmotte@users.noreply.github.com>
2026-03-01 17:54:08 +0100

    docs(check-filenames): clarify checked text content

--- scripts/cicd/check-filenames.sh
+++ scripts/cicd/check-filenames.sh
@@ -5,9 +5,12 @@ set -e
 readonly main_dir=${1:?}; shift
 readonly grep_args=("$@")
 
-if [ "$USE_GIT_LS_FILES" = true ]
-    then filenames=$(git -C "$main_dir" ls-files)
-    else filenames=$(find "$main_dir" \
+if [ "$USE_GIT_LS_FILES" = true ]; then
+    # Relative paths of all the files (NO directories)
+    filenames=$(git -C "$main_dir" ls-files)
+else
+    # Basenames of all the files AND directories
+    filenames=$(find "$main_dir" \
         \! -name .git \! -path '*/.git/*' -printf '%f\n')
 fi
 
