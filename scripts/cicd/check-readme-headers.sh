a24a0bf6116989390c60aec22182e97f4278d650
dmotte <37443982+dmotte@users.noreply.github.com>
2026-03-06 13:52:50 +0100

    feat(check-readme-headers): add support for additional ls args

--- scripts/cicd/check-readme-headers.sh
+++ scripts/cicd/check-readme-headers.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-readonly main_dir=${1:-.}
+readonly main_dir=${1:?}; shift
+readonly add_ls_args=("$@")
 
 # We need to work with absolute paths, otherwise this script would fail in
 # case a "./README.md" file exists
@@ -10,11 +11,12 @@ realpath_main_dir=$(realpath "$main_dir")
 
 if [ "$USE_GIT_LS_FILES" = true ]; then
     files=$(git -C "$main_dir" ls-files \
-        ':(icase)README.md' ':(icase)*/README.md')
+        ':(icase)README.md' ':(icase)*/README.md' "${add_ls_args[@]}")
     files=$(printf '%s' "$files" | while IFS= read -r i || [ -n "$i" ]; do
         echo "$realpath_main_dir/$i"; done)
 else
-    files=$(find "$realpath_main_dir" -type f -iname README.md)
+    files=$(find "$realpath_main_dir" -type f -iname README.md \
+        "${add_ls_args[@]}")
 fi
 
 while IFS= read -r i || [ -n "$i" ]; do
