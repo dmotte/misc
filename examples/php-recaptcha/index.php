a4a1fb5440a664824ed5b98c03c7df5fc207f671
dmotte <37443982+dmotte@users.noreply.github.com>
2026-03-20 18:13:52 +0100

    style: fix HTML head and some other stuff

--- examples/php-recaptcha/index.php
+++ examples/php-recaptcha/index.php
@@ -1,38 +1,43 @@
+<!doctype html>
 <html>
 
 <head>
-    <title>reCAPTCHA v2 PHP Demo</title>
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <title>reCAPTCHA v2 PHP Demo</title>
+
+  <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 
 <body>
-    <?php
-    $recaptchaSecretKey = "SECRET_KEY_HERE";
+  <?php
+  $recaptchaSecretKey = "SECRET_KEY_HERE";
 
-    if (isset($_POST['g-recaptcha-response'])) {
-        $username = $_POST['username'];
-        $responseKey = $_POST['g-recaptcha-response'];
-        $remoteIP = $_SERVER['REMOTE_ADDR'];
+  if (isset($_POST['g-recaptcha-response'])) {
+    $username = $_POST['username'];
+    $responseKey = $_POST['g-recaptcha-response'];
+    $remoteIP = $_SERVER['REMOTE_ADDR'];
 
-        $recaptchaResponseRaw = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$recaptchaSecretKey&response=$responseKey&remoteip=$remoteIP");
-        $recaptchaResponseObj = json_decode($recaptchaResponseRaw);
+    $recaptchaResponseRaw = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$recaptchaSecretKey&response=$responseKey&remoteip=$remoteIP");
+    $recaptchaResponseObj = json_decode($recaptchaResponseRaw);
 
-        echo "<pre>$recaptchaResponseRaw</pre>";
+    echo "<pre>$recaptchaResponseRaw</pre>";
 
-        if ($recaptchaResponseObj->success)
-            echo "<p>Verification succeeded. Username: <b>$username</b></p>";
-        else
-            echo "<p>Verification failed!</p>";
-    }
-    ?>
+    if ($recaptchaResponseObj->success)
+      echo "<p>Verification succeeded. Username: <b>$username</b></p>";
+    else
+      echo "<p>Verification failed!</p>";
+  }
+  ?>
 
-    <form method="POST">
-        Your name: <input type="text" name="username" /><br />
-        <br />
-        <div class="g-recaptcha" data-sitekey="SITE_KEY_HERE"></div>
-        <br />
-        <input type="submit" value="Submit" />
-    </form>
+  <form method="POST">
+    Your name: <input type="text" name="username" /><br />
+    <br />
+    <div class="g-recaptcha" data-sitekey="SITE_KEY_HERE"></div>
+    <br />
+    <input type="submit" value="Submit" />
+  </form>
 </body>
 
 </html>
--- php-scripts/php-funnel/funnel.php
+++ php-scripts/php-funnel/funnel.php
@@ -107,13 +107,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html>
 
 <head>
-    <title>Funnel</title>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>Funnel</title>
 
     <style type="text/css">
         body,
