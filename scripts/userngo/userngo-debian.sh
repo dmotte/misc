d6a060bbcffdfcb9f4bc94a8f662c695690361ff
dmotte <37443982+dmotte@users.noreply.github.com>
2026-06-21 15:42:31 +0200

    chore(userngo): draft userngo-debian.sh complete

--- scripts/userngo/userngo-debian.sh
+++ scripts/userngo/userngo-debian.sh
@@ -12,10 +12,10 @@ fi
 readonly psw=$USERNGO_PSW
 unset USERNGO_PSW
 
-[ "$scenario" = root ] && [ -n "$psw" ] &&
-    { echo "Setting the root user's password"; echo "root:$psw" | chpasswd; }
+if [ "$scenario" = root ] && [ -n "$psw" ]
+    then echo 'Setting root password'; echo "root:$psw" | chpasswd; fi
 
-[ "$scenario" = managed ] || { echo 'Running main app'; exec "$@"; }
+if [ "$scenario" != managed ]; then echo 'Running main app'; exec "$@"; fi
 
 IFS=: read -ra parts <<< "$USERNGO_ID"
 readonly id_user=${parts[0]:-auto}
@@ -30,28 +30,43 @@ readonly sys_group=${parts[1]:-$sys_user}
 readonly sudoer=$USERNGO_SUDOER
 readonly nopasswd=$USERNGO_NOPASSWD
 
-{ getent passwd "$id_user" || getent passwd "$name_user"; } >/dev/null || {
-    useradd_args=() # TODO build the args list and use it
+if ! { getent passwd "$id_user" ||
+        getent passwd "$name_user"; } >/dev/null; then
+    add_args_user=()
 
-    { getent group "$id_group" || getent group "$name_group"; } >/dev/null || {
-        echo "Creating group $name_group ($id_group)"
-        groupadd -g"$id_group" "$name_group"
-    }
+    if [ "$id_user:$name_user" = "$id_group:$name_group" ]; then
+        add_args_user+=(-U)
+    else
+        add_args_group=()
 
-    # TODO
-}
+        [ "$id_group" = auto ] || add_args_group+=(-g"$id_group")
+        [ "$sys_group" != true ] || add_args_group+=(-r)
 
-# TODO INSPIRATION: useradd -m -s /bin/bash -u "$id_user" -g "$id_group" "$name_user"
-echo "Creating user $name_user"
-useradd -UGsudo -ms/bin/bash "$name_user"
+        echo "Creating group $name_group (ID $id_group)"
+        groupadd "${add_args_group[@]}" "$name_group"
 
-echo "Setting the user's password"
-echo "$name_user:$psw" | chpasswd
+        add_args_user+=(-g"$id_group")
+    fi
 
-if [ "$nopasswd" = true ]; then
-    echo "Enabling sudo without password for user $name_user"
-    install -Tm440 <(echo "$name_user ALL=(ALL) NOPASSWD: ALL") \
-        "/etc/sudoers.d/$name_user-nopassword"
+    [ "$id_user" = auto ] || add_args_user+=(-u"$id_user")
+    [ "$sys_user" != true ] || add_args_user+=(-r)
+    [ "$sudoer" != true ] || add_args_user+=(-Gsudo)
+
+    shell=$(getent passwd root | cut -d: -f7)
+
+    echo "Creating user $name_user (ID $id_user)"
+    useradd "${add_args_user[@]}" -ms"$shell" "$name_user"
+
+    if [ -n "$psw" ]; then
+        echo "Setting password for user $name_user"
+        echo "$name_user:$psw" | chpasswd
+    fi
+
+    if [ "$nopasswd" = true ]; then
+        echo "Enabling sudo without password for user $name_user"
+        install -Tm440 <(echo "$name_user ALL=(ALL) NOPASSWD: ALL") \
+            "/etc/sudoers.d/$name_user-nopasswd"
+    fi
 fi
 
 echo "Running main app as $id_user:$id_group"
