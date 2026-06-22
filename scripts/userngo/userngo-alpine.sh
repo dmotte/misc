48cbab625eff50f875989ded4a10c4b3e8a06ed0
dmotte <37443982+dmotte@users.noreply.github.com>
2026-06-22 18:23:52 +0200

    style(userngo): add prefix to echo msgs

--- scripts/userngo/userngo-alpine.sh
+++ scripts/userngo/userngo-alpine.sh
@@ -13,9 +13,10 @@ readonly psw=$USERNGO_PSW
 unset USERNGO_PSW
 
 if [ "$scenario" = root ] && [ -n "$psw" ]
-    then echo 'Setting root password'; echo "root:$psw" | chpasswd; fi
+    then echo 'userngo: setting root password'; echo "root:$psw" | chpasswd; fi
 
-if [ "$scenario" != managed ]; then echo 'Running main app'; exec "$@"; fi
+if [ "$scenario" != managed ]
+    then echo 'userngo: running main app'; exec "$@"; fi
 
 IFS=: read -ra parts <<< "$USERNGO_ID"
 readonly id_user=${parts[0]:-auto}
@@ -41,7 +42,7 @@ if ! getent passwd "$name_user" >/dev/null; then
         [ "$id_group" = auto ] || add_args_group+=(-g"$id_group")
         [ "$sys_group" != true ] || add_args_group+=(-S)
 
-        echo "Creating group $name_group (ID $id_group)"
+        echo "userngo: creating group $name_group (ID $id_group)"
         addgroup "${add_args_group[@]}" "$name_group"
 
         add_args_user+=(-G"$name_group")
@@ -50,25 +51,25 @@ if ! getent passwd "$name_user" >/dev/null; then
     [ "$id_user" = auto ] || add_args_user+=(-u"$id_user")
     [ "$sys_user" != true ] || add_args_user+=(-S)
 
-    echo "Creating user $name_user (ID $id_user)"
+    echo "userngo: creating user $name_user (ID $id_user)"
     adduser "${add_args_user[@]}" -Ds"$shell" "$name_user"
 
     if [ -n "$psw" ]; then
-        echo "Setting password for user $name_user"
+        echo "userngo: setting password for user $name_user"
         echo "$name_user:$psw" | chpasswd
     fi
 
     if [ "$wheel" = true ]; then
-        echo "Adding user $name_user to the wheel group"
+        echo "userngo: adding user $name_user to the wheel group"
         addgroup "$name_user" wheel
     fi
 
     if [ "$nopass" = true ]; then
-        echo "Enabling doas without password for user $name_user"
+        echo "userngo: enabling doas without password for user $name_user"
         echo "permit nopass $name_user" \
             > "/etc/doas.d/50-$name_user-nopass.conf"
     fi
 fi
 
-echo "Running main app as $name_user:$name_group"
+echo "userngo: running main app as $name_user:$name_group"
 exec su-exec "$name_user:$name_group" "$@"
--- scripts/userngo/userngo-debian.sh
+++ scripts/userngo/userngo-debian.sh
@@ -13,9 +13,10 @@ readonly psw=$USERNGO_PSW
 unset USERNGO_PSW
 
 if [ "$scenario" = root ] && [ -n "$psw" ]
-    then echo 'Setting root password'; echo "root:$psw" | chpasswd; fi
+    then echo 'userngo: setting root password'; echo "root:$psw" | chpasswd; fi
 
-if [ "$scenario" != managed ]; then echo 'Running main app'; exec "$@"; fi
+if [ "$scenario" != managed ]
+    then echo 'userngo: running main app'; exec "$@"; fi
 
 IFS=: read -ra parts <<< "$USERNGO_ID"
 readonly id_user=${parts[0]:-auto}
@@ -42,7 +43,7 @@ if ! getent passwd "$name_user" >/dev/null; then
         [ "$id_group" = auto ] || add_args_group+=(-g"$id_group")
         [ "$sys_group" != true ] || add_args_group+=(-r)
 
-        echo "Creating group $name_group (ID $id_group)"
+        echo "userngo: creating group $name_group (ID $id_group)"
         groupadd "${add_args_group[@]}" "$name_group"
 
         add_args_user+=(-g"$name_group")
@@ -52,20 +53,20 @@ if ! getent passwd "$name_user" >/dev/null; then
     [ "$sys_user" != true ] || add_args_user+=(-r)
     [ "$sudoer" != true ] || add_args_user+=(-Gsudo)
 
-    echo "Creating user $name_user (ID $id_user)"
+    echo "userngo: creating user $name_user (ID $id_user)"
     useradd "${add_args_user[@]}" -ms"$shell" "$name_user"
 
     if [ -n "$psw" ]; then
-        echo "Setting password for user $name_user"
+        echo "userngo: setting password for user $name_user"
         echo "$name_user:$psw" | chpasswd
     fi
 
     if [ "$nopasswd" = true ]; then
-        echo "Enabling sudo without password for user $name_user"
+        echo "userngo: enabling sudo without password for user $name_user"
         install -Tm440 <(echo "$name_user ALL=(ALL) NOPASSWD: ALL") \
             "/etc/sudoers.d/$name_user-nopasswd"
     fi
 fi
 
-echo "Running main app as $name_user:$name_group"
+echo "userngo: running main app as $name_user:$name_group"
 exec gosu "$name_user:$name_group" "$@"
