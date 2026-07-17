fe538090f1cffc3de2d5f55b5229363cd622b7f2
dmotte <37443982+dmotte@users.noreply.github.com>
2026-07-17 15:00:41 +0200

    fix(sshset): use args in app.sh

--- scripts/sshset/app.sh
+++ scripts/sshset/app.sh
@@ -5,8 +5,8 @@ set -e
 bash /opt/sshset/main.sh
 
 # if [ "$EUID" = 0 ]
-#     then exec /usr/sbin/sshd -De
-#     else exec /usr/sbin/sshd -Def ~/.ssh/sshd_config
+#     then exec /usr/sbin/sshd -De "$@"
+#     else exec /usr/sbin/sshd -Def ~/.ssh/sshd_config "$@"
 # fi
 
-exec /bin/bash
+exec /bin/bash "$@"
