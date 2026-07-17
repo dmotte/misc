b39987ca9cd4f512760f4ae40fe22cd25c1fc9d3
dmotte <37443982+dmotte@users.noreply.github.com>
2026-07-17 15:03:48 +0200

    chore(sshset): add client command to app.sh

--- scripts/sshset/app.sh
+++ scripts/sshset/app.sh
@@ -4,9 +4,13 @@ set -e
 
 bash /opt/sshset/main.sh
 
+################################################################################
+
 # if [ "$EUID" = 0 ]
 #     then exec /usr/sbin/sshd -De "$@"
 #     else exec /usr/sbin/sshd -Def ~/.ssh/sshd_config "$@"
 # fi
+
+# exec /usr/bin/ssh "$@"
 
 exec /bin/bash "$@"
