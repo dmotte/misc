a27d12e4e49bb3c1454009100af4e362f5b788a0
dmotte <37443982+dmotte@users.noreply.github.com>
2026-07-25 01:02:03 +0200

    fix(sshset): create .ssh dir owned by user

--- scripts/sshset/sshset.sh
+++ scripts/sshset/sshset.sh
@@ -120,7 +120,7 @@ if [ "$EUID" = 0 ]; then
         user_group=$(id -gn "$user") # This fails if the user doesn't exist
         user_home=$(getent passwd "$user" | cut -d: -f6)
 
-        install -dvm700 "$user_home/.ssh"
+        install -o"$user" -g"$user_group" -dvm700 "$user_home/.ssh"
 
         ########################################################################
 
